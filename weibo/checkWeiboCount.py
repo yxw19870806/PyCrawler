4d1fdea021fd92ca96075b5fe746ef215dc83a6d
hikaru <hikaru870806@hotmail.com>
2015-10-11 00:38:03 +0800

    检测文件名字

--- weibo/checkWeiboCount.py
+++ weibo/checkWeiboCount.py
@@ -17,13 +17,15 @@ def getCount(idPath, imageRootPath):
         line = line.split("\t")
         imagePath = imageRootPath + line[1]
         imagePath = imagePath.decode('UTF-8').encode('GBK')
+        all_file = os.listdir(imagePath)
         if os.path.exists(imagePath):
-            count1 = len(os.listdir(imagePath))
+            count1 = len(all_file)
         else:
             count1 = 0
         count2 = int(line[2])
-        if count1 != count2:
-            print line[1] + ": " + str(count1) + ", " + str(count2)
+        max_count = int(max(all_file).split('.')[0])
+        if count1 != count2 or count1 != max_count:
+            print line[1] + ": " + str(count1) + ", " + str(count2) + ", " + str(max_count)
     print "check over!"
 
 getCount("info\\idlist_1.txt", "Z:\\G+\\weibo\\weibo1\\")
