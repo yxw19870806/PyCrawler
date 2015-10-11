b271e12398c48c675f24a7b23731947607cc3d64
hikaru <hikaru870806@hotmail.com>
2015-10-11 18:08:27 +0800

    删除原本没有使用多线程的下载代码

--- twitter/Twitter.py
+++ twitter/Twitter.py
@@ -128,7 +128,8 @@ class Twitter(common.Robot):
         new_user_id_list_file = open(new_user_id_list_file_path, "w")
         new_user_id_list_file.close()
 
-        total_image_count = 0
+        TOTAL_IMAGE_COUNT = 0
+
         # 循环下载每个id
         for user_account in sorted(user_id_list.keys()):
             # 检查正在运行的线程数
@@ -143,143 +144,6 @@ class Twitter(common.Robot):
             # 开始下载
             thread = Download(user_id_list[user_account])
             thread.start()
-            # print_step_msg("Account: " + user_account)
-            #
-            # # 初始化数据
-            # last_image_url = user_id_list[user_account][2]
-            # user_id_list[user_account][2] = ''  # 置空，存放此次的最后URL
-            # data_tweet_id = init_max_id
-            # image_count = 1
-            # image_url_list = []
-            # is_pass = False
-            # is_last_page = False
-            # # 如果有存档记录，则直到找到与前一次一致的地址，否则都算有异常
-            # if last_image_url != '':
-            #     is_error = True
-            # else:
-            #     is_error = False
-            # # 如果需要重新排序则使用临时文件夹，否则直接下载到目标目录
-            # if self.is_sort == 1:
-            #     image_path = self.image_temp_path
-            # else:
-            #     image_path = self.image_download_path + "\\" + user_account
-            # if not common.make_dir(image_path, 1):
-            #     print_error_msg("创建图片下载目录： " + image_path + " 失败，程序结束！")
-            #     common.process_exit()
-            #
-            # # 图片下载
-            # while not is_last_page:
-            #     if is_pass:
-            #         break
-            #     photo_page_url = "https://twitter.com/i/profiles/show/%s/media_timeline?include_available_features=1&include_entities=1&max_position=%s" % (user_account, data_tweet_id)
-            #     photo_page_data = self.do_get(photo_page_url)
-            #     if not photo_page_data:
-            #         print_error_msg("无法获取相册信息: " + photo_page_url)
-            #         break
-            #     try:
-            #         page = json.read(photo_page_data)
-            #     except:
-            #         print_error_msg("返回信息：" + str(photo_page_data) + " 不是一个JSON数据, account: " + user_account)
-            #         break
-            #
-            #     if not isinstance(page, dict):
-            #         print_error_msg("JSON数据：" + str(page) + " 不是一个字典, account: " + user_account)
-            #         break
-            #     if not page.has_key("has_more_items"):
-            #         print_error_msg("在JSON数据：" + str(page) + " 中没有找到'has_more_items'字段, account: " + user_account)
-            #         break
-            #     if not page['has_more_items']:
-            #         is_last_page = True
-            #     if page.has_key("items_html") is False:
-            #         print_error_msg("在JSON数据：" + str(page) + " 中没有找到'items_html'字段, account: " + user_account)
-            #         break
-            #
-            #     page = page['items_html']
-            #
-            #     image_index = page.find("data-url")
-            #     while image_index != -1:
-            #         image_start = page.find("http", image_index)
-            #         image_stop = page.find('"', image_start)
-            #         image_url = page[image_start:image_stop].encode("utf-8")
-            #         if image_url.find('&quot') != -1:
-            #             image_url = image_url[:image_url.find('&quot')]
-            #         trace("image URL:" + image_url)
-            #         # 将第一张image的URL保存到新id list中
-            #         if user_id_list[user_account][2] == "":
-            #             user_id_list[user_account][2] = image_url
-            #         # 检查是否已下载到前一次的图片
-            #         if image_url == last_image_url:
-            #             is_pass = True
-            #             is_error = False
-            #             break
-            #         if image_url in image_url_list:
-            #             image_index = page.find('data-url', image_index + 1)
-            #             continue
-            #         image_url_list.append(image_url)
-            #         print_step_msg("开始下载第 " + str(image_count) + "张图片：" + image_url)
-            #         img_byte = common.do_get(image_url)
-            #         if img_byte:
-            #             # 文件类型
-            #             file_type = image_url.split(".")[-1].split(':')[0]
-            #             # 保存图片
-            #             image_file = open(image_path + "\\" + str("%04d" % image_count) + "." + file_type, "wb")
-            #             image_file.write(img_byte)
-            #             print_step_msg("下载成功")
-            #             image_file.close()
-            #             image_count += 1
-            #         else:
-            #             print_error_msg("获取第" + str(image_count) + "张图片信息失败：" + user_account + "：" + image_url)
-            #
-            #         # 达到配置文件中的下载数量，结束
-            #         if last_image_url != '' and self.get_image_count > 0 and image_count > self.get_image_count:
-            #             is_pass = True
-            #             break
-            #         image_index = page.find('data-url', image_index + 1)
-            #
-            #     if not is_last_page:
-            #         # 设置最后一张的data-tweet-id
-            #         data_tweet_id_index = page.find('data-tweet-id="')
-            #         while data_tweet_id_index != -1:
-            #             data_tweet_id_start = page.find('"', data_tweet_id_index)
-            #             data_tweet_id_stop = page.find('"', data_tweet_id_start + 1)
-            #             data_tweet_id = page[data_tweet_id_start + 1:data_tweet_id_stop]
-            #             data_tweet_id_index = page.find('data-tweet-id="', data_tweet_id_index + 1)
-            #
-            # user_id_list[user_account][1] = str(int(user_id_list[user_account][1]) + image_count - 1)
-            # total_image_count += image_count - 1
-            #
-            # print_step_msg(user_account + "下载完毕，总共获得" + str(image_count - 1) + "张图片")
-            #
-            # # 排序
-            # if IS_SORT == 1:
-            #     image_list = sorted(os.listdir(image_path), reverse=True)
-            #     # 判断排序目标文件夹是否存在
-            #     if len(image_list) >= 1:
-            #         destination_path = self.image_download_path + "\\" + user_account
-            #         if not common.make_dir(destination_path, 1):
-            #             print_error_msg("创建图片子目录： " + destination_path + " 失败，程序结束！")
-            #             common.process_exit()
-            #
-            #         # 倒叙排列
-            #         if len(user_id_list[user_account]) >= 2 and user_id_list[user_account][1] != '':
-            #             count = int(user_id_list[user_account][1]) + 1
-            #         else:
-            #             count = 1
-            #         for file_name in image_list:
-            #             file_type = file_name.split(".")[1]
-            #             common.copy_files(image_path + "\\" + file_name, destination_path + "\\" + str("%04d" % count) + "." + file_type)
-            #             count += 1
-            #         print_step_msg("图片从下载目录移动到保存目录成功")
-            #     # 删除临时文件夹
-            #     common.remove_dir(image_path)
-            #
-            # if is_error:
-            #     print_error_msg(user_account + "图片数量异常，请手动检查")
-
-            # # 保存最后的信息
-            # new_user_id_list_file = open(new_user_id_list_file_path, "a")
-            # new_user_id_list_file.write("\t".join(user_id_list[user_account]) + "\n")
-            # new_user_id_list_file.close()
 
         # 检查所有线程是不是全部结束了
         while THREAD_COUNT != 0:
@@ -289,7 +153,7 @@ class Twitter(common.Robot):
         common.remove_dir(IMAGE_TEMP_PATH)
 
         stop_time = time.time()
-        print_step_msg("存档文件中所有用户图片已成功下载，耗时" + str(int(stop_time - start_time)) + "秒，共计图片" + str(total_image_count) + "张")
+        print_step_msg("存档文件中所有用户图片已成功下载，耗时" + str(int(stop_time - start_time)) + "秒，共计图片" + str(TOTAL_IMAGE_COUNT) + "张")
 
 
 class Download(threading.Thread):
