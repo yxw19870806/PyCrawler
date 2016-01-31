0a95991b718625d1cf9e829183723fb76b27d6fd
hikaru <hikaru870806@hotmail.com>
2016-01-31 19:10:39 +0800

    fix splitext自带.的问题

--- common/robot.py
+++ common/robot.py
@@ -130,7 +130,7 @@ def sort_file(source_path, destination_path, start_count, file_name_length):
         for file_name in image_list:
             start_count += 1
             file_type = os.path.splitext(file_name)[1]
-            new_file_name = str(("%0" + str(file_name_length) + "d") % start_count) + "." + file_type
+            new_file_name = str(("%0" + str(file_name_length) + "d") % start_count) + file_type
             tool.copy_files(os.path.join(source_path, file_name), os.path.join(destination_path, new_file_name))
         # 删除临时文件夹
         tool.remove_dir(source_path)
