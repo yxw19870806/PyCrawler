fd86f170292c9a211a8c9eebbc7e65656c99bc27
hikaru <hikaru870806@hotmail.com>
2016-02-15 00:31:56 +0800

    发生错误时的数据复原

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -352,6 +352,10 @@ class Download(threading.Thread):
                     # 全部图片下载完毕
                     break
 
+            # 如果有错误且没有发现新的图片，复原旧数据
+            if self.user_info[3] == "0" and int(last_image_time) > 0:
+                self.user_info[3] = last_image_time
+
             print_step_msg(user_name + " 下载完毕，总共获得" + str(image_count - 1) + "张图片")
 
             # 排序
