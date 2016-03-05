9a2267e9476a736cd502f7d6764c486865a1d21c
hikaru <hikaru870806@hotmail.com>
2016-03-05 11:55:26 +0800

    增加设置进程结束状态的运行入口

---
+++ main/stop.py
@@ -0,0 +1,11 @@
+# -*- coding:UTF-8  -*-
+'''
+Created on 2013-7-16
+
+@author: Administrator
+'''
+
+import os
+
+root_path = os.path.abspath('..')
+open(os.path.join(root_path, 'stop'), 'w').close()
