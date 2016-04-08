9fcb4573fc4f0a1b4b5899cfe78735fc8d006ca6
hikaru <hikaru870806@hotmail.com>
2016-04-08 23:22:54 +0800

    取消错误提示

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -190,10 +190,10 @@ class Download(threading.Thread):
             image_url_list = []
             is_over = False
             # 如果有存档记录，则直到找到在记录之前的图片，否则都算错误
-            if last_image_time == "0":
-                is_error = False
-            else:
-                is_error = True
+            # if last_image_time == "0":
+            #     is_error = False
+            # else:
+            #     is_error = True
             need_make_download_dir = True
 
             # 如果需要重新排序则使用临时文件夹，否则直接下载到目标目录
@@ -248,7 +248,7 @@ class Download(threading.Thread):
                         # 检查是否已下载到前一次的图片
                         if 0 < int(last_image_time) >= image_time:
                             is_over = True
-                            is_error = False
+                            # is_error = False
                             break
 
                         # 文件类型
@@ -272,7 +272,7 @@ class Download(threading.Thread):
                     # 达到配置文件中的下载数量，结束
                     if 0 < GET_IMAGE_COUNT < image_count:
                         is_over = True
-                        is_error = False
+                        # is_error = False
                         break
 
                 if is_over:
@@ -301,8 +301,8 @@ class Download(threading.Thread):
 
             self.user_info[1] = str(int(self.user_info[1]) + image_count - 1)
 
-            if is_error:
-                print_error_msg(user_account + " 图片数量异常，请手动检查")
+            # if is_error:
+            #     print_error_msg(user_account + " 图片数量异常，请手动检查")
 
             # 保存最后的信息
             threadLock.acquire()
