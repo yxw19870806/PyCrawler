68f6da6116b1cb8c695f7a35d6ea94cb929466f3
hikaru <hikaru870806@hotmail.com>
2016-06-06 17:13:11 +0800

    只使用相关域名的cookie

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -156,7 +156,7 @@ class Weibo(robot.Robot):
             tool.set_proxy(self.proxy_ip, self.proxy_port, "http")
 
         # 设置系统cookies
-        if not tool.set_cookie(self.cookie_path, self.browser_version):
+        if not tool.set_cookie(self.cookie_path, self.browser_version, ('weibo.com', '.sina.com.cn')):
             print_error_msg("导入浏览器cookies失败，程序结束！")
             tool.process_exit()
 
