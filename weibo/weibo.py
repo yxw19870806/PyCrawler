32ac6b9637f70ce7a50161a50968c9d1344da785
hikaru <hikaru870806@hotmail.com>
2016-07-04 17:16:22 +0800

    微博增加配置视频下载数量

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -203,6 +203,7 @@ class Weibo(robot.Robot):
     def __init__(self, save_data_path="", this_image_download_path="", this_image_temp_path="",
                  this_video_download_path="", this_video_temp_path=""):
         global GET_IMAGE_COUNT
+        global GET_VIDEO_COUNT
         global IMAGE_TEMP_PATH
         global IMAGE_DOWNLOAD_PATH
         global VIDEO_TEMP_PATH
@@ -218,6 +219,7 @@ class Weibo(robot.Robot):
             self.save_data_path = save_data_path
 
         GET_IMAGE_COUNT = self.get_image_count
+        GET_VIDEO_COUNT = self.get_video_count
         if this_image_temp_path != "":
             IMAGE_TEMP_PATH = this_image_temp_path
         else:
@@ -417,6 +419,11 @@ class Download(threading.Thread):
                             video_count += 1
                         else:
                             print_error_msg(account_name + " 第" + str(video_count) + "个视频 " + video_page_url + " 下载失败")
+
+                    # 达到配置文件中的下载数量，结束
+                    if 0 < GET_VIDEO_COUNT < video_count:
+                        is_over = True
+                        break
 
                 if not is_over:
                     since_id_data = re.findall('action-data="type=video&owner_uid=&since_id=([\d]*)">', video_page_data)
