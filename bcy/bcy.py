bf933dfe981accd255e6b864dd457544370c5f49
hikaru <hikaru870806@hotmail.com>
2016-07-04 16:02:24 +0800

    update

--- bcy/bcy.py
+++ bcy/bcy.py
@@ -273,40 +273,43 @@ class Download(threading.Thread):
 
                     rp_url = "http://bcy.net/coser/detail/%s/%s" % (cp_id, rp_id)
                     [rp_page_return_code, rp_page_response] = tool.http_request(rp_url)[:2]
-                    if rp_page_return_code == 1:
-                        image_count = 0
-                        image_url_list = re.findall("src='([^']*)'", rp_page_response)
-                        if len(image_url_list) == 0:
-                            print_step_msg(cn + " 检测到可能有私密作品且账号不是ta的粉丝，自动关注")
-                            if follow(coser_id):
-                                # 重新获取下详细页面
-                                rp_url = "http://bcy.net/coser/detail/%s/%s" % (cp_id, rp_id)
-                                [rp_page_return_code, rp_page_response] = tool.http_request(rp_url)[:2]
-                                if rp_page_return_code == 1:
-                                    image_url_list = re.findall("src='([^']*)'", rp_page_response)
+                    if rp_page_return_code != 1:
+                        print_error_msg(cn + " 无法获取正片页面： " + rp_url)
+                        continue
 
-                        if len(image_url_list) == 0:
-                            print_error_msg(cn + " " + rp_id + " 没有任何图片，可能是你使用的账号没有关注ta，所以无法访问只对粉丝开放的私密作品")
-                            continue
+                    image_url_list = re.findall("src='([^']*)'", rp_page_response)
+                    if len(image_url_list) == 0:
+                        print_step_msg(cn + " 检测到可能有私密作品且账号不是ta的粉丝，自动关注")
+                        if follow(coser_id):
+                            # 重新获取下详细页面
+                            rp_url = "http://bcy.net/coser/detail/%s/%s" % (cp_id, rp_id)
+                            [rp_page_return_code, rp_page_response] = tool.http_request(rp_url)[:2]
+                            if rp_page_return_code == 1:
+                                image_url_list = re.findall("src='([^']*)'", rp_page_response)
 
-                        for image_url in image_url_list:
+                    if len(image_url_list) == 0:
+                        print_error_msg(cn + " " + rp_id + " 没有任何图片，可能是你使用的账号没有关注ta，所以无法访问只对粉丝开放的私密作品")
+                        continue
+
+                    image_count = 1
+                    for image_url in image_url_list:
+                        # 禁用指定分辨率
+                        image_url = "/".join(image_url.split("/")[0:-1])
+
+                        if image_url.rfind("/") < image_url.rfind("."):
+                            file_type = image_url.split(".")[-1]
+                        else:
+                            file_type = "jpg"
+                        file_path = os.path.join(rp_path, str("%03d" % image_count) + "." + file_type)
+
+                        print_step_msg(cn + ":" + rp_id + " 开始下载第" + str(image_count) + "张图片：" + image_url)
+                        if tool.save_image(image_url, file_path):
                             image_count += 1
-                            # 禁用指定分辨率
-                            image_url = "/".join(image_url.split("/")[0:-1])
+                            print_step_msg(cn + " " + rp_id + " 第" + str(image_count) + "张图片下载成功")
+                        else:
+                            print_error_msg(cn + " " + rp_id + " 第" + str(image_count) + "张图片 " + image_url + " 下载失败")
 
-                            if image_url.rfind("/") < image_url.rfind("."):
-                                file_type = image_url.split(".")[-1]
-                            else:
-                                file_type = "jpg"
-                            file_path = os.path.join(rp_path, str("%03d" % image_count) + "." + file_type)
-
-                            print_step_msg(cn + ":" + rp_id + " 开始下载第" + str(image_count) + "张图片：" + image_url)
-                            if tool.save_image(image_url, file_path):
-                                print_step_msg(cn + " " + rp_id + " 第" + str(image_count) + "张图片下载成功")
-                            else:
-                                print_error_msg(cn + " " + rp_id + " 第" + str(image_count) + "张图片 " + image_url + " 下载失败")
-
-                        this_cn_total_image_count += image_count - 1
+                    this_cn_total_image_count += image_count - 1
 
                     title_index += 1
 
