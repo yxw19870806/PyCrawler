4e3fb83d6a04dca8c62cd32f7ee4ce40367aafb5
hikaru <hikaru870806@hotmail.com>
2016-07-19 16:20:56 +0800

    update

--- main/main.py
+++ main/main.py
@@ -3,17 +3,18 @@
 from common import tool
 import os
 
+
 # 半次元
 def bcy():
     from bcy import bcy
-    bcy_path = os.path.join(os.path.abspath('..'), 'bcy')
+    bcy_path = os.path.join(os.path.abspath(".."), "bcy")
     os.chdir(bcy_path)
     bcy.Bcy().main()
 
 # fkoji
 def fkoji():
     from fkoji import fkoji
-    fkoji_path = os.path.join(os.path.abspath('..'), 'fkoji')
+    fkoji_path = os.path.join(os.path.abspath(".."), "fkoji")
     os.chdir(fkoji_path)
     fkoji.Fkoji().main()
 
@@ -21,7 +22,7 @@ def fkoji():
 # GooglePlus
 def google_plus():
     from googlePlus import googlePlus
-    google_plus_path = os.path.join(os.path.abspath('..'), 'googlePlus')
+    google_plus_path = os.path.join(os.path.abspath(".."), "googlePlus")
     os.chdir(google_plus_path)
     googlePlus.GooglePlus().main()
 
@@ -29,7 +30,7 @@ def google_plus():
 # Instagram
 def instagram():
     from instagram import instagram
-    instagram_path = os.path.join(os.path.abspath('..'), 'instagram')
+    instagram_path = os.path.join(os.path.abspath(".."), "instagram")
     os.chdir(instagram_path)
     instagram.Instagram().main()
 
@@ -37,7 +38,7 @@ def instagram():
 # Lofter
 def lofter():
     from lofter import lofter
-    lofter_path = os.path.join(os.path.abspath('..'), 'lofter')
+    lofter_path = os.path.join(os.path.abspath(".."), "lofter")
     os.chdir(lofter_path)
     lofter.Lofter().main()
 
@@ -45,7 +46,7 @@ def lofter():
 # tumblr
 def tumblr():
     from tumblr import tumblr
-    tumblr_path = os.path.join(os.path.abspath('..'), 'tumblr')
+    tumblr_path = os.path.join(os.path.abspath(".."), "tumblr")
     os.chdir(tumblr_path)
     tumblr.Tumblr().main()
 
@@ -53,21 +54,24 @@ def tumblr():
 # # Twitter
 def twitter():
     from twitter import twitter
-    twitter_path = os.path.join(os.path.abspath('..'), 'twitter')
+    twitter_path = os.path.join(os.path.abspath(".."), "twitter")
     os.chdir(twitter_path)
     for i in range(1, 4):
-        save_file_name = 'info\\save_%s.data' % i
-        image_download_dir_name = 'photo\\twitter%s' % i
+        save_file_name = "info\\save_%s.data" % i
+        image_download_dir_name = "photo\\twitter%s" % i
         save_file_path = os.path.join(twitter_path, save_file_name)
         image_download_path = os.path.join(twitter_path, image_download_dir_name)
-        image_temp_path = os.path.join(image_download_path, 'tempImage')
-        twitter.Twitter(save_file_path, image_download_path, image_temp_path).main()
+        image_temp_path = os.path.join(image_download_path, "tempImage")
+        video_download_dir_name = "video\\twitter%s" % i
+        video_download_path = os.path.join(os.path.abspath(""), video_download_dir_name)
+        video_temp_path = os.path.join(video_download_path, "tempVideo")
+        twitter.Twitter(save_file_path, image_download_path, image_temp_path, video_download_path, video_temp_path).main()
 
 
 # # Weibo
 def weibo():
     from weibo import weibo
-    weibo_path = os.path.join(os.path.abspath('..'), 'weibo')
+    weibo_path = os.path.join(os.path.abspath(".."), "weibo")
     os.chdir(weibo_path)
     for save_file in ["ATF", "lunar", "save_1", "save_2", "snh48"]:
         save_file_name = "info\\%s.data" % save_file
