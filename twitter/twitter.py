8fc86df3a2a4816723940f1bf6005197826c2ebc
hikaru <hikaru870806@hotmail.com>
2016-07-26 14:38:06 +0800

    判断是否开启下载图片或视频

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -253,17 +253,25 @@ class Twitter(robot.Robot):
     def main(self):
         global ACCOUNTS
         
-        if IS_DOWNLOAD_IMAGE == 0:
-            print_error_msg("下载图片没开启，请检查配置！")
+        if IS_DOWNLOAD_IMAGE == 0 and IS_DOWNLOAD_VIDEO == 0:
+            print_error_msg("下载图片和视频都没开启，请检查配置！")
             tool.process_exit()
 
         start_time = time.time()
 
-        # 图片保存目录
-        print_step_msg("创建图片根目录：" + IMAGE_DOWNLOAD_PATH)
-        if not tool.make_dir(IMAGE_DOWNLOAD_PATH, 0):
-            print_error_msg("创建图片根目录：" + IMAGE_DOWNLOAD_PATH + " 失败")
-            tool.process_exit()
+        # 创建图片保存目录
+        if IS_DOWNLOAD_IMAGE == 1:
+            print_step_msg("创建图片根目录：" + IMAGE_DOWNLOAD_PATH)
+            if not tool.make_dir(IMAGE_DOWNLOAD_PATH, 0):
+                print_error_msg("创建图片根目录：" + IMAGE_DOWNLOAD_PATH + " 失败")
+                tool.process_exit()
+
+        # 创建视频保存目录
+        if IS_DOWNLOAD_VIDEO == 1:
+            print_step_msg("创建视频根目录：" + VIDEO_DOWNLOAD_PATH)
+            if not tool.make_dir(VIDEO_DOWNLOAD_PATH, 0):
+                print_error_msg("创建视频根目录：" + VIDEO_DOWNLOAD_PATH + " 失败")
+                tool.process_exit()
 
         # 设置代理
         if self.is_proxy == 1 or self.is_proxy == 2:
