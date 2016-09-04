3937b72dcdeb603f30cff3555a55b0a672ca333a
hikaru <hikaru870806@hotmail.com>
2016-09-04 22:14:00 +0800

    update

--- weibo/checkWeiboCount.py
+++ weibo/checkWeiboCount.py
@@ -13,18 +13,19 @@ def getCount(idPath, imageRootPath):
     tempFile.close()
     for line in lines:
         line = line.split("\t")
-        imagePath = imageRootPath + line[1]
+        name= line[5].replace("\n", "")
+        imagePath = imageRootPath + name
         imagePath = imagePath.decode('UTF-8').encode('GBK')
         all_file = os.listdir(imagePath)
         if os.path.exists(imagePath):
             count1 = len(all_file)
         else:
             count1 = 0
-        count2 = int(line[2])
+        count2 = int(line[1])
         max_count = int(max(all_file).split('.')[0])
         if count1 != count2 or count1 != max_count:
-            print line[1] + ": " + str(count1) + ", " + str(count2) + ", " + str(max_count)
+            print name + ": " + str(count1) + ", " + str(count2) + ", " + str(max_count)
     print "check over!"
 
-for save_file in ["ATF", "lunar", "save_1", "save_2", "snh48"]:
-    getCount("info\\%s.data" % save_file, "Z:\\G+\\weibo\\%s\\" % save_file)
+for save_file in ["ATF", "lunar", "save_1",]:
+    getCount("info\\%s.data" % save_file, "E:\\weibo\\photo\\%s\\" % save_file)
