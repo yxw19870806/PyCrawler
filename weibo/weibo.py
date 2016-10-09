e232c6236fce204209c2e53e3260650c4a729cef
hikaru <hikaru870806@hotmail.com>
2016-10-09 11:26:29 +0800

    是否排序的判断修改

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -554,14 +554,14 @@ class Download(threading.Thread):
 
             # 排序
             if IS_SORT:
-                if image_count > 1:
+                if first_image_time != "0":
                     destination_path = os.path.join(IMAGE_DOWNLOAD_PATH, account_name)
                     if robot.sort_file(image_path, destination_path, int(self.account_info[1]), 4):
                         print_step_msg(account_name + " 图片从下载目录移动到保存目录成功")
                     else:
                         print_error_msg(account_name + " 创建图片保存目录 %s 失败" % destination_path)
                         tool.process_exit()
-                if video_count > 1:
+                if first_video_url != "":
                     destination_path = os.path.join(VIDEO_DOWNLOAD_PATH, account_name)
                     if robot.sort_file(video_path, destination_path, int(self.account_info[3]), 4):
                         print_step_msg(account_name + " 视频从下载目录移动到保存目录成功")
