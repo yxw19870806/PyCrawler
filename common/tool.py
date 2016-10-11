5059420f4eb54a5309be18d6078a212cd8bdda7b
hikaru <hikaru870806@hotmail.com>
2016-10-11 17:07:35 +0800

    新增压缩&解压缩 方法

--- common/tool.py
+++ common/tool.py
@@ -18,6 +18,7 @@ import threading
 import traceback
 import urllib
 import urllib2
+import zipfile
 
 IS_SET_TIMEOUT = False
 PROCESS_STATUS = 0
@@ -542,6 +543,47 @@ def copy_files(source_path, destination_path):
     source_path = change_path_encoding(source_path)
     destination_path = change_path_encoding(destination_path)
     shutil.copyfile(source_path, destination_path)
+
+
+# 压缩文件夹
+# need_source_dir   是否需要把文件夹名作为根目录
+def zip_dir(source_dir, zip_file_path, need_source_dir=True):
+    file_list = []
+    path_prefix_len = len(source_dir) # 文件列表路径前缀的长度
+    # 是目录，直接添加
+    if os.path.isfile(source_dir):
+        file_list.append(source_dir)
+    else :
+        # 如果需要包含目录
+        if need_source_dir:
+            path_prefix_len -= len(os.path.basename(source_dir)) + 1
+        for root, dirs, files in os.walk(source_dir):
+            for name in files:
+                file_list.append(os.path.join(root, name))
+    zip_file = zipfile.ZipFile(zip_file_path, "w", zipfile.zlib.DEFLATED)
+    for file_path in file_list:
+        zip_file_path = file_path[path_prefix_len:]
+        zip_file.write(file_path, zip_file_path)
+    zip_file.close()
+
+
+# 解压缩文件
+def unzip_file(zip_file_path, destination_path):
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    zip_file = zipfile.ZipFile(zip_file_path)
+    for zip_file_path in zip_file.namelist():
+        zip_file_path = zip_file_path.replace('\\','/')
+        if zip_file_path.endswith('/'):  # 解压目录
+            os.makedirs(os.path.join(destination_path, zip_file_path))
+        else:  # 解压文件
+            file_path = os.path.join(destination_path, zip_file_path)  # 文件的完整路径
+            file_dir = os.path.dirname(file_path)
+            if not os.path.exists(file_dir):
+                os.makedirs(file_dir)
+            outfile = open(file_path, 'wb')
+            outfile.write(zip_file.read(zip_file_path))
+            outfile.close()
 
 
 # 生成指定长度的随机字符串
