7ffedd1722c4346f520c87a30bc3b0113405714c
hikaru <hikaru870806@hotmail.com>
2016-10-17 20:02:41 +0800

    注释

--- main/continue.py
+++ main/continue.py
@@ -1,5 +1,6 @@
 # -*- coding:UTF-8  -*-
 """
+继续所有已经暂停的爬虫程序
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
--- main/pause.py
+++ main/pause.py
@@ -1,5 +1,6 @@
 # -*- coding:UTF-8  -*-
 """
+暂停所有正在运行的爬虫程序
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
