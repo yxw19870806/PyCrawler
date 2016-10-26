eb840ef1e273ee39d9a0e1a984543639505e5cac
hikaru <hikaru870806@hotmail.com>
2016-10-26 14:21:16 +0800

    fix字符串查找首尾同的情况下返回空字符串

--- common/tool.py
+++ common/tool.py
@@ -393,7 +393,7 @@ def find_sub_string(string, start_string=None, end_string=None, include_string=0
         if end_string is None:
             stop_index = len(string)
         else:
-            stop_index = string.find(end_string, start_index)
+            stop_index = string.find(end_string, start_index + 1)
         if stop_index >= 0:
             if include_string & 2 == 2:
                 stop_index += len(end_string)
