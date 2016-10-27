007db86a952535fd15e3ac58641ecfb3b3f3e240
hikaru <hikaru870806@hotmail.com>
2016-10-27 12:24:03 +0800

    状态设置

--- main/pause.py
+++ main/pause.py
@@ -5,6 +5,6 @@
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
-from common import process, tool
+from common import process
 
-process.set_process_status(tool.ProcessControl.PROCESS_PAUSE)
+process.set_process_status(process.PROCESS_STATUS_PAUSE)
