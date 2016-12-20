58f24f9cf6c23e732e2fcb0940197f99530761fd
hikaru <hikaru870806@hotmail.com>
2016-12-20 19:59:03 +0800

    分组移动封面图到子目录中

---
+++ designation/group.py
@@ -0,0 +1,32 @@
+# -*- coding:UTF-8  -*-
+"""
+分组移动封面图到子目录中
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+import os
+import shutil
+
+
+def group():
+    # 没有分类的
+    other_dir = os.path.join(destination_path, "-")
+    if os.path.exists(other_dir):
+        os.makedirs(other_dir)
+
+    for root, dirs, files in os.walk(source_path):
+        for file_name in files:
+            if file_name.find("-") >= 0:
+                title, num = file_name.split("-")
+                new_dir = os.path.join(destination_path, title)
+                if not os.path.exists(new_dir):
+                    os.makedirs(new_dir)
+                shutil.move(os.path.join(root, file_name), os.path.join(new_dir, file_name))
+            else:
+                shutil.move(os.path.join(root, file_name), os.path.join(other_dir, file_name))
+
+if __name__ == "__main__":
+    source_path = "C:\\Users\\Administrator\\Documents\\photo"
+    destination_path = "C:\\Users\\Administrator\\Documents\\group"
+    group()
