4934fdfd821f193f1b5e831a4ba36d5925c3f380
hikaru <hikaru870806@hotmail.com>
2016-12-27 00:54:40 +0800

    不存在文件夹的创建 & windows保留的文件夹名

--- designation/group.py
+++ designation/group.py
@@ -12,21 +12,24 @@ import shutil
 def group():
     # 没有分类的
     other_dir = os.path.join(destination_path, "-")
-    if os.path.exists(other_dir):
+    if not os.path.exists(other_dir):
         os.makedirs(other_dir)
 
     for root, dirs, files in os.walk(source_path):
         for file_name in files:
             if file_name.find("-") >= 0:
                 title, num = file_name.split("-")
-                new_dir = os.path.join(destination_path, title)
-                if not os.path.exists(new_dir):
-                    os.makedirs(new_dir)
-                shutil.move(os.path.join(root, file_name), os.path.join(new_dir, file_name))
-            else:
-                shutil.move(os.path.join(root, file_name), os.path.join(other_dir, file_name))
+                if title.isalpha():
+                    if title == "CON":
+                        title = "CON_"
+                    new_dir = os.path.join(destination_path, title)
+                    if not os.path.exists(new_dir):
+                        os.makedirs(new_dir)
+                    shutil.move(os.path.join(root, file_name), os.path.join(new_dir, file_name))
+                    continue
+            shutil.move(os.path.join(root, file_name), os.path.join(other_dir, file_name))
 
 if __name__ == "__main__":
-    source_path = "C:\\Users\\Administrator\\Documents\\photo"
-    destination_path = "C:\\Users\\Administrator\\Documents\\group"
+    source_path = "D:\\workspace\\1\\photo"
+    destination_path = "D:\\workspace\\2"
     group()
