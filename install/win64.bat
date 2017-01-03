481c6eab087f6248c99bce6d0683aeb581700e2a
hikaru <hikaru870806@hotmail.com>
2017-01-03 15:06:26 +0800

    PIL 的安装脚本

--- install/win64.bat
+++ install/win64.bat
@@ -13,7 +13,10 @@ echo install pyHook
 C:\python27\Scripts\pip.exe install pyHook-1.5.1-cp27-cp27m-win_amd64.whl
 ::C:\python27\Scripts\pip.exe install http://www.lfd.uci.edu/~gohlke/pythonlibs/f9r7rmd8/pyHook-1.5.1-cp27-cp27m-win_amd64.whl
 
-echo install urllib3
-C:\python27\Scripts\pip.exe install ..\urllib3-1.19.1-py2.py3-none-any.whl
+::echo install urllib3
+::C:\python27\Scripts\pip.exe install ..\urllib3-1.19.1-py2.py3-none-any.whl
+
+::echo install urllib3
+::C:\python27\Scripts\pip.exe install ..\Pillow-4.0.0-cp27-cp27m-win_amd64.whl
 
 pause
