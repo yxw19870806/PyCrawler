eab0b4cd2b66ddad49825333f622cf76e7266d0f
hikaru <hikaru870806@hotmail.com>
2017-01-08 23:13:03 +0800

    下载读取存档&完成后写入存档&下载文件名

--- ywkb/ywkb.py
+++ ywkb/ywkb.py
@@ -9,67 +9,47 @@ from common import log, robot, tool
 import os
 import re
 
-ACCOUNTS = []
-TOTAL_IMAGE_COUNT = 0
-GET_IMAGE_COUNT = 0
-IMAGE_TEMP_PATH = ""
-IMAGE_DOWNLOAD_PATH = ""
-NEW_SAVE_DATA_PATH = ""
-IS_SORT = True
-IS_DOWNLOAD_IMAGE = True
 
-
-# 获取一页图片列表
-def get_one_page_image_url_list(page_count):
+# 获取一页图片信息列表
+def get_one_page_image_info_list(page_count):
     index_url = "http://www.dahuadan.com/category/ywkb/page/%s" % page_count
     index_response = tool.http_request2(index_url)
     if index_response.status == 200:
-        image_url_list = re.findall('<img class="aligncenter" src="([^"]*)" />', index_response.data)
-        return {"is_over": False, "image_url_list": image_url_list}
+        article_data = tool.find_sub_string(index_response.data, '<section id="primary"', "</section>")
+        image_info_list = re.findall('<article id="post-([\d]*)"[\s|\S]*?<img class="aligncenter" src="([^"]*)" />', article_data)
+        return {"is_over": False, "image_info_list": image_info_list}
     elif index_response.status == 404:
-        return {"is_over": True, "image_url_list": []}
+        return {"is_over": True, "image_info_list": []}
     return None
 
 
 class Template(robot.Robot):
     def __init__(self):
-        global GET_IMAGE_COUNT
-        global IMAGE_TEMP_PATH
-        global IMAGE_DOWNLOAD_PATH
-        global NEW_SAVE_DATA_PATH
-        global IS_SORT
-        global IS_DOWNLOAD_IMAGE
-
         sys_config = {
             robot.SYS_DOWNLOAD_IMAGE: True,
             robot.SYS_NOT_CHECK_SAVE_DATA: True,
         }
         robot.Robot.__init__(self, sys_config, use_urllib3=True)
 
-        # 设置全局变量，供子线程调用
-        GET_IMAGE_COUNT = self.get_image_count
-        IMAGE_TEMP_PATH = self.image_temp_path
-        IMAGE_DOWNLOAD_PATH = self.image_download_path
-        IS_SORT = self.is_sort
-        IS_DOWNLOAD_IMAGE = self.is_download_image
-        NEW_SAVE_DATA_PATH = robot.get_new_save_file_path(self.save_data_path)
-
     def main(self):
-        global ACCOUNTS
+        save_post_id = 1
+        if os.path.exists(self.save_data_path):
+            save_file = open(self.save_data_path, "r")
+            save_info = save_file.read()
+            save_file.close()
+            save_post_id = int(save_info.strip())
 
         # 如果需要重新排序则使用临时文件夹，否则直接下载到目标目录
-        if IS_SORT:
-            image_path = IMAGE_TEMP_PATH
-        else:
-            image_path = IMAGE_DOWNLOAD_PATH
-        tool.make_dir(image_path, 0)
+        tool.make_dir(self.image_download_path, 0)
 
         page_count = 1
         image_count = 1
-        while True:
+        first_post_id = 0
+        is_over = True
+        while is_over:
             log.step("开始解析第%s页日志" % page_count)
 
-            image_info = get_one_page_image_url_list(page_count)
+            image_info = get_one_page_image_info_list(page_count)
             if image_info is None:
                 log.error(" 第%s页图片无法获取" % page_count)
                 tool.process_exit()
@@ -77,20 +57,38 @@ class Template(robot.Robot):
             if image_info["is_over"]:
                 break
 
-            for image_url in image_info["image_url_list"]:
-                log.step("开始下载第%s张图片 %s" % (page_count, image_url))
+            for post_id, image_url in image_info["image_info_list"]:
+                post_id = int(post_id)
+                # 检查是否图片时间小于上次的记录
+                if post_id <= save_post_id:
+                    is_over = True
+                    break
+
+                # 将第一张图片的post id做为新的存档记录
+                if first_post_id == 0:
+                    first_post_id = post_id
+
+                log.step("开始下载%s的图片 %s" % (post_id, image_url))
 
                 file_type = image_url.split(".")[-1]
-                file_path = os.path.join(image_path, "%04d.%s" % (image_count, file_type))
+                file_path = os.path.join(self.image_download_path, "%04d.%s" % (post_id, file_type))
                 if tool.save_net_file2(image_url, file_path):
-                    log.step("第%s张图片下载成功" % image_count)
+                    log.step("%s的图片下载成功" % post_id)
                     image_count += 1
                 else:
-                    log.error("第%s张图片 %s 获取失败" % (image_count, image_url))
+                    log.error("%s的图片 %s 获取失败" % (post_id, image_url))
 
             page_count += 1
 
         log.step("全部下载完毕，耗时%s秒，共计图片%s张" % (self.get_run_time(), image_count - 1))
+
+        # 重新保存存档文件
+        save_data_dir = os.path.dirname(self.save_data_path)
+        if not os.path.exists(save_data_dir):
+            tool.make_dir(save_data_dir, 0)
+        save_file = open(self.save_data_path, "w")
+        save_file.write(str(first_post_id))
+        save_file.close()
 
 
 if __name__ == "__main__":
