6f947b4190a0d55aac0fb1e04477c53e4c61f36f
hikaru <hikaru870806@hotmail.com>
2017-01-09 16:12:02 +0800

    update

--- game/clickHero/autoClick.py
+++ game/clickHero/autoClick.py
@@ -1,16 +1,17 @@
 # -*- coding:UTF-8  -*-
 """
-clicker heroes自动升级树精
+clicker heroes自动点击怪物
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 import time
+
 import clickerHeroesAutoClick
 
 
 # 自动点击怪物
 if __name__ == "__main__":
     while True:
-        clickerHeroesAutoClick.auto_click(100, 370)
-        time.sleep(1)
+        clickerHeroesAutoClick.auto_click(865, 410)
+        time.sleep(0.01)
--- game/clickHero/autoUpgrade.py
+++ game/clickHero/autoUpgrade.py
@@ -1,16 +1,17 @@
 # -*- coding:UTF-8  -*-
 """
-clicker heroes自动点击怪物
+clicker heroes自动升级树精
 @author: hikaru
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 import time
+
 import clickerHeroesAutoClick
 
 
 # 升级树精等级，默认窗口大小，滚动条在最上方
 if __name__ == "__main__":
     while True:
-        clickerHeroesAutoClick.auto_click(865, 410)
-        time.sleep(0.01)
+        clickerHeroesAutoClick.auto_click(100, 370)
+        time.sleep(1)
