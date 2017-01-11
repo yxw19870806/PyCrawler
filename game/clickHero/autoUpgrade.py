26f89688fb1b738d7f1e69562585b33fa09a041a
hikaru <hikaru870806@hotmail.com>
2017-01-11 11:40:29 +0800

    检测宝箱频率降低

--- game/clickHero/autoUpgrade.py
+++ game/clickHero/autoUpgrade.py
@@ -19,16 +19,16 @@ if __name__ == "__main__":
     count = 0
     while True:
         # 自动升级
-        # 判断指定制定坐标，是不是出现了装备宝箱，如果出现了就打开并关闭弹出界面
-        red, green, blue = ch.get_color(570, 410)
-        if red == 255 and 250 <= green <= 253 and 190 <= blue <= 210:
-            ch.auto_click(575, 370)
-            time.sleep(1)
-            ch.auto_click(920, 125)
-            print "open equip box"
-        # 每隔60秒检查一次是否启动了自动通关模式（战斗失败会自动关闭）
         ch.auto_click(click_x, click_y)
+        # 每隔10分钟秒检查一次是否启动了自动通关模式（战斗失败会自动关闭）
         if count >= 60:
+            # 判断指定制定坐标，是不是出现了装备宝箱，如果出现了就打开并关闭弹出界面
+            red, green, blue = ch.get_color(570, 410)
+            if red == 255 and 250 <= green <= 253 and 190 <= blue <= 210:
+                ch.auto_click(575, 370)
+                time.sleep(1)
+                ch.auto_click(920, 125)
+                print "open equip box"
             is_find = False
             for pos_x in range(1110, 1130):
                 for pos_y in range(240, 260):
