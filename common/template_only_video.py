880971c868a16d42e25c67c19f0d6db5aae203cb
hikaru <hikaru870806@hotmail.com>
2017-01-16 11:33:54 +0800

    单独模板

---
+++ common/template_only_video.py
@@ -0,0 +1,162 @@
+# -*- coding:UTF-8  -*-
+"""
+模板
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+from common import log, robot, tool
+import json
+import os
+import re
+import threading
+import time
+import traceback
+
+ACCOUNTS = []
+TOTAL_VIDEO_COUNT = 0
+GET_VIDEO_COUNT = 0
+VIDEO_TEMP_PATH = ""
+VIDEO_DOWNLOAD_PATH = ""
+NEW_SAVE_DATA_PATH = ""
+IS_SORT = True
+
+
+class Template(robot.Robot):
+    def __init__(self):
+        global GET_VIDEO_COUNT
+        global VIDEO_TEMP_PATH
+        global VIDEO_DOWNLOAD_PATH
+        global NEW_SAVE_DATA_PATH
+        global IS_SORT
+
+        # todo 配置
+        sys_config = {
+            robot.SYS_DOWNLOAD_VIDEO: True,
+            robot.SYS_SET_PROXY: True,
+            robot.SYS_NOT_CHECK_SAVE_DATA: True,
+            robot.SYS_SET_COOKIE: ("example.com",)
+        }
+        robot.Robot.__init__(self, sys_config)
+
+        # 设置全局变量，供子线程调用
+        GET_VIDEO_COUNT = self.get_video_count
+        VIDEO_TEMP_PATH = self.video_temp_path
+        VIDEO_DOWNLOAD_PATH = self.video_download_path
+        IS_SORT = self.is_sort
+        NEW_SAVE_DATA_PATH = robot.get_new_save_file_path(self.save_data_path)
+
+    def main(self):
+        global ACCOUNTS
+
+        # todo 存档文件格式
+        # 解析存档文件
+        # account_id
+        account_list = robot.read_save_data(self.save_data_path, 0, ["", ])
+        ACCOUNTS = account_list.keys()
+
+        # 循环下载每个id
+        main_thread_count = threading.activeCount()
+        for account_id in sorted(account_list.keys()):
+            # 检查正在运行的线程数
+            while threading.activeCount() >= self.thread_count + main_thread_count:
+                if robot.is_process_end() == 0:
+                    time.sleep(10)
+                else:
+                    break
+
+            # 提前结束
+            if robot.is_process_end() > 0:
+                break
+
+            # 开始下载
+            thread = Download(account_list[account_id], self.thread_lock)
+            thread.start()
+
+            time.sleep(1)
+
+        # 检查除主线程外的其他所有线程是不是全部结束了
+        while threading.activeCount() > main_thread_count:
+            time.sleep(10)
+
+        # 未完成的数据保存
+        if len(ACCOUNTS) > 0:
+            new_save_data_file = open(NEW_SAVE_DATA_PATH, "a")
+            for account_id in ACCOUNTS:
+                new_save_data_file.write("\t".join(account_list[account_id]) + "\n")
+            new_save_data_file.close()
+
+        # 删除临时文件夹
+        self.finish_task()
+
+        # 重新排序保存存档文件
+        robot.rewrite_save_file(NEW_SAVE_DATA_PATH, self.save_data_path)
+
+        log.step("全部下载完毕，耗时%s秒，共计视频%s个" % (self.get_run_time(), TOTAL_VIDEO_COUNT))
+
+
+class Download(threading.Thread):
+    def __init__(self, account_info, thread_lock):
+        threading.Thread.__init__(self)
+        self.account_info = account_info
+        self.thread_lock = thread_lock
+
+    def run(self):
+        global TOTAL_VIDEO_COUNT
+
+        account_id = self.account_info[0]
+        # todo 是否有需要显示不同名字
+        account_name = account_id
+
+        try:
+            log.step(account_name + " 开始")
+
+            # 如果需要重新排序则使用临时文件夹，否则直接下载到目标目录
+            if IS_SORT:
+                video_path = os.path.join(VIDEO_TEMP_PATH, account_name)
+            else:
+                video_path = os.path.join(VIDEO_DOWNLOAD_PATH, account_name)
+
+            # todo 视频下载逻辑
+            # 视频
+            video_count = 1
+            first_video_time = "0"
+            need_make_video_dir = True
+
+            log.step(account_name + " 下载完毕，总共获得%s个视频" % (video_count - 1))
+
+            # 排序
+            if IS_SORT:
+                if first_video_time != "0":
+                    destination_path = os.path.join(VIDEO_DOWNLOAD_PATH, account_name)
+                    if robot.sort_file(video_path, destination_path, int(self.account_info[3]), 4):
+                        log.step(account_name + " 视频从下载目录移动到保存目录成功")
+                    else:
+                        log.error(account_name + " 创建视频保存目录 %s 失败" % destination_path)
+                        tool.process_exit()
+
+            # 新的存档记录
+            if first_video_time != "0":
+                self.account_info[3] = str(int(self.account_info[3]) + video_count - 1)
+                self.account_info[4] = first_video_time
+
+            # 保存最后的信息
+            tool.write_file("\t".join(self.account_info), NEW_SAVE_DATA_PATH)
+            self.thread_lock.acquire()
+            TOTAL_VIDEO_COUNT += video_count - 1
+            ACCOUNTS.remove(account_id)
+            self.thread_lock.release()
+
+            log.step(account_name + " 完成")
+        except SystemExit, se:
+            if se.code == 0:
+                log.step(account_name + " 提前退出")
+            else:
+                log.error(account_name + " 异常退出")
+        except Exception, e:
+            log.error(account_name + " 未知异常")
+            log.error(str(e) + "\n" + str(traceback.format_exc()))
+
+
+if __name__ == "__main__":
+    Template().main()
