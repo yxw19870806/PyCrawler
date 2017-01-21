474eda42e704bd156d9cdd496b51f2e9b2409ddc
hikaru <hikaru870806@hotmail.com>
2017-01-22 00:07:00 +0800

    update

--- bcy/bcy.py
+++ bcy/bcy.py
@@ -136,48 +136,48 @@ def unfollow(account_id):
 
 
 # 获取一页的作品信息
-def get_one_page_post(coser_id, page_count):
+def get_one_page_post(account_id, page_count):
     # http://bcy.net/u/50220/post/cos?&p=1
-    post_url = "http://bcy.net/u/%s/post/cos?&p=%s" % (coser_id, page_count)
+    post_url = "http://bcy.net/u/%s/post/cos?&p=%s" % (account_id, page_count)
     return net.http_request(post_url)
 
 
-# 解析作品信息，获取所有的正片信息
-def get_rp_list(post_page):
-    cp_and_rp_id_list = re.findall('/coser/detail/(\d+)/(\d+)"', post_page)
+# 解析作品信息，获取所有的作品信息
+def get_album_list(post_page):
+    cp_and_album_id_list = re.findall('/coser/detail/(\d+)/(\d+)"', post_page)
     title_list = re.findall('<img src="\S*" alt="([\S ]*)" />', post_page)
     if "${post.title}" in title_list:
         title_list.remove("${post.title}")
-    cp_id = None
-    rp_list = {}
-    if len(cp_and_rp_id_list) == len(title_list):
-        for cp_id, rp_id in cp_and_rp_id_list:
-            rp_list[rp_id] = title_list.pop(0)
-    return cp_id, rp_list
+    coser_id = None
+    album_list = {}
+    if len(cp_and_album_id_list) == len(title_list):
+        for coser_id, album_id in cp_and_album_id_list:
+            album_list[album_id] = title_list.pop(0)
+    return coser_id, album_list
 
 
-# 获取正片页面
-def get_rp_data(cp_id, rp_id):
+# 获取作品页面
+# coser_id -> 9299
+# album_id -> 36484
+def get_album_page(coser_id, album_id):
     # http://bcy.net/coser/detail/9299/36484
-    rp_url = "http://bcy.net/coser/detail/%s/%s" % (cp_id, rp_id)
-    return net.http_request(rp_url)
+    album_page_url = "http://bcy.net/coser/detail/%s/%s" % (coser_id, album_id)
+    return net.http_request(album_page_url)
 
 
-# 检测正片是否被管理员锁定
-def is_invalid_rp(rp_data):
-    return rp_data.find("该作品属于下属违规情况，已被管理员锁定：") >= 0
+# 检测作品是否被管理员锁定
+def is_invalid_album(album_page):
+    return album_page.find("该作品属于下属违规情况，已被管理员锁定：") >= 0
 
 
-# 获取正片页面内的所有图片地址列表
-# cp_id -> 9299
-# rp_id -> 36484
-def get_image_url_list(rp_data):
-    return re.findall("src='([^']*)'", rp_data)
+# 获取作品页面内的所有图片地址列表
+def get_image_url_list(album_page):
+    return re.findall("src='([^']*)'", album_page)
 
 
 # 根据当前作品页面，获取作品页数上限
-def get_max_page_count(coser_id, post_page):
-    max_page_count = tool.find_sub_string(post_page, '<a href="/u/%s/post/cos?&p=' % coser_id, '">')
+def get_max_page_count(account_id, post_page):
+    max_page_count = tool.find_sub_string(post_page, '<a href="/u/%s/post/cos?&p=' % account_id, '">')
     if max_page_count:
         max_page_count = int(max_page_count)
     else:
@@ -229,7 +229,7 @@ class Bcy(robot.Robot):
                         break
 
         # 解析存档文件
-        # account_id  last_rp_id
+        # account_id  last_album_id
         account_list = robot.read_save_data(self.save_data_path, 0, ["", "0"])
         ACCOUNTS = account_list.keys()
 
@@ -279,158 +279,158 @@ class Download(threading.Thread):
     def run(self):
         global TOTAL_IMAGE_COUNT
 
-        coser_id = self.account_info[0]
+        account_id = self.account_info[0]
         if len(self.account_info) >= 3:
-            cn = self.account_info[2]
+            account_name = self.account_info[2]
         else:
-            cn = self.account_info[0]
+            account_name = self.account_info[0]
 
         try:
-            log.step(cn + " 开始")
+            log.step(account_name + " 开始")
 
-            image_path = os.path.join(IMAGE_DOWNLOAD_PATH, cn)
+            image_path = os.path.join(IMAGE_DOWNLOAD_PATH, account_name)
 
             # 图片下载
-            this_cn_total_image_count = 0
+            this_account_total_image_count = 0
             page_count = 1
-            total_rp_count = 1
-            first_rp_id = ""
+            total_album_count = 1
+            first_album_id = ""
             unique_list = []
             is_over = False
             need_make_download_dir = True
             while not is_over:
-                log.step(cn + " 开始解析第%s页作品" % page_count)
+                log.step(account_name + " 开始解析第%s页作品" % page_count)
 
                 # 获取一页的作品信息
-                post_page_response = get_one_page_post(coser_id, page_count)
+                post_page_response = get_one_page_post(account_id, page_count)
                 if post_page_response.status != 200:
-                    log.error(cn + " 第%s页作品访问失败，原因：%s" % (page_count, robot.get_http_request_failed_reason(post_page_response.status)))
+                    log.error(account_name + " 第%s页作品访问失败，原因：%s" % (page_count, robot.get_http_request_failed_reason(post_page_response.status)))
                     tool.process_exit()
 
-                # 解析作品信息，获取所有的正片信息
-                cp_id, rp_list = get_rp_list(post_page_response.data)
-                if cp_id is None:
-                    log.error(cn + " 第%s页作品解析异常" % page_count)
+                # 解析作品信息，获取所有的作品信息
+                coser_id, album_list = get_album_list(post_page_response.data)
+                if coser_id is None:
+                    log.error(account_name + " 第%s页作品解析异常" % page_count)
                     tool.process_exit()
-                log.trace(cn + " cp_id：%s" % cp_id)
-                log.trace(cn + " 第%s页获取的所有作品：%s" % (page_count, rp_list))
+                log.trace(account_name + " coser id：%s" % coser_id)
+                log.trace(account_name + " 第%s页获取的所有作品：%s" % (page_count, album_list))
 
-                for rp_id, title in rp_list.iteritems():
+                for album_id, title in album_list.iteritems():
                     # 检查是否已下载到前一次的图片
-                    if int(rp_id) <= int(self.account_info[1]):
+                    if int(album_id) <= int(self.account_info[1]):
                         is_over = True
                         break
 
                     # 将第一个作品的id做为新的存档记录
-                    if first_rp_id == "":
-                        first_rp_id = rp_id
+                    if first_album_id == "":
+                        first_album_id = album_id
 
-                    # 新增正片导致的重复判断
-                    if rp_id in unique_list:
+                    # 新增作品导致的重复判断
+                    if album_id in unique_list:
                         continue
                     else:
-                        unique_list.append(rp_id)
+                        unique_list.append(album_id)
 
-                    log.step(cn + " 开始解析作品%s 《%s》" % (rp_id, title))
+                    log.step(account_name + " 开始解析作品%s 《%s》" % (album_id, title))
 
                     if need_make_download_dir:
                         if not tool.make_dir(image_path, 0):
-                            log.error(cn + " 创建CN目录 %s 失败" % image_path)
+                            log.error(account_name + " 创建下载目录 %s 失败" % image_path)
                             tool.process_exit()
                         need_make_download_dir = False
 
                     # 过滤标题中不支持的字符
                     filtered_title = robot.filter_text(title)
                     if filtered_title:
-                        rp_path = os.path.join(image_path, "%s %s" % (rp_id, filtered_title))
+                        album_path = os.path.join(image_path, "%s %s" % (album_id, filtered_title))
                     else:
-                        rp_path = os.path.join(image_path, rp_id)
-                    if not tool.make_dir(rp_path, 0):
+                        album_path = os.path.join(image_path, album_id)
+                    if not tool.make_dir(album_path, 0):
                         # 目录出错，把title去掉后再试一次，如果还不行退出
-                        log.error(cn + " 创建作品目录 %s 失败，尝试不使用title" % rp_path)
-                        rp_path = os.path.join(image_path, rp_id)
-                        if not tool.make_dir(rp_path, 0):
-                            log.error(cn + " 创建作品目录 %s 失败" % rp_path)
+                        log.error(account_name + " 创建作品目录 %s 失败，尝试不使用title" % album_path)
+                        album_path = os.path.join(image_path, album_id)
+                        if not tool.make_dir(album_path, 0):
+                            log.error(account_name + " 创建作品目录 %s 失败" % album_path)
                             tool.process_exit()
 
-                    # 获取正片页面
-                    rp_page_response = get_rp_data(cp_id, rp_id)
-                    if rp_page_response.status != 200:
-                        log.error(cn + " 作品%s 《%s》（cp_id：%s）访问失败，原因：%s" % (rp_id, cp_id, title, robot.get_http_request_failed_reason(rp_page_response.status)))
+                    # 获取作品页面
+                    album_page_response = get_album_page(coser_id, album_id)
+                    if album_page_response.status != 200:
+                        log.error(account_name + " 作品%s 《%s》（coser_id：%s）访问失败，原因：%s" % (album_id, coser_id, title, robot.get_http_request_failed_reason(album_page_response.status)))
                         tool.process_exit()
 
-                    if is_invalid_rp(rp_page_response.data):
-                        log.error(cn + " 作品%s 《%s》（cp_id：%s）已被管理员锁定，跳过" % (rp_id, title, cp_id))
+                    if is_invalid_album(album_page_response.data):
+                        log.error(account_name + " 作品%s 《%s》（coser_id：%s）已被管理员锁定，跳过" % (album_id, title, coser_id))
                         continue
 
                     # 获取作品中的全部图片地址
-                    image_url_list = get_image_url_list(rp_page_response.data)
+                    image_url_list = get_image_url_list(album_page_response.data)
 
                     if len(image_url_list) == 0 and IS_AUTO_FOLLOW:
-                        log.step(cn + " 检测到可能有私密作品且账号不是ta的粉丝，自动关注")
-                        if follow(coser_id):
-                            # 重新获取正片页面
-                            rp_page_response = get_rp_data(cp_id, rp_id)
-                            if rp_page_response.status != 200:
-                                log.error(cn + " 作品%s 《%s》（cp_id：%s）访问失败，原因：%s" % (rp_id, title, cp_id, robot.get_http_request_failed_reason(rp_page_response.status)))
+                        log.step(account_name + " 检测到可能有私密作品且账号不是ta的粉丝，自动关注")
+                        if follow(account_id):
+                            # 重新获取作品页面
+                            album_page_response = get_album_page(coser_id, album_id)
+                            if album_page_response.status != 200:
+                                log.error(account_name + " 作品%s 《%s》（coser_id：%s）访问失败，原因：%s" % (album_id, title, coser_id, robot.get_http_request_failed_reason(album_page_response.status)))
                                 tool.process_exit()
                             # 重新获取作品中的全部图片地址
-                            image_url_list = get_image_url_list(rp_page_response.data)
+                            image_url_list = get_image_url_list(album_page_response.data)
 
                     if len(image_url_list) == 0:
-                        log.error(cn + " 作品%s 《%s》（cp_id：%s）没有任何图片，可能是你使用的账号没有关注ta，所以无法访问只对粉丝开放的私密作品" % (rp_id, title, cp_id))
+                        log.error(account_name + " 作品%s 《%s》（coser_id：%s）没有任何图片，可能是你使用的账号没有关注ta，所以无法访问只对粉丝开放的私密作品" % (album_id, title, coser_id))
                         continue
 
                     image_count = 1
                     for image_url in list(image_url_list):
                         # 禁用指定分辨率
                         image_url = "/".join(image_url.split("/")[0:-1])
-                        log.step(cn + " %s 《%s》开始下载第%s张图片 %s" % (rp_id, title, image_count, image_url))
+                        log.step(account_name + " %s 《%s》开始下载第%s张图片 %s" % (album_id, title, image_count, image_url))
 
                         if image_url.rfind("/") < image_url.rfind("."):
                             file_type = image_url.split(".")[-1]
                         else:
                             file_type = "jpg"
-                        file_path = os.path.join(rp_path, "%03d.%s" % (image_count, file_type))
+                        file_path = os.path.join(album_path, "%03d.%s" % (image_count, file_type))
                         save_file_return = net.save_net_file(image_url, file_path)
                         if save_file_return["status"] == 1:
-                            log.step(cn + " %s 《%s》第%s张图片下载成功" % (rp_id, title, image_count))
+                            log.step(account_name + " %s 《%s》第%s张图片下载成功" % (album_id, title, image_count))
                             image_count += 1
                         else:
-                            log.error(" %s 《%s》第%s张图片 %s，下载失败，原因：%s" % (rp_id, title, image_count, image_url, robot.get_save_net_file_failed_reason(save_file_return["code"])))
+                            log.error(" %s 《%s》第%s张图片 %s，下载失败，原因：%s" % (album_id, title, image_count, image_url, robot.get_save_net_file_failed_reason(save_file_return["code"])))
 
-                    this_cn_total_image_count += image_count - 1
+                    this_account_total_image_count += image_count - 1
 
-                    if 0 < GET_PAGE_COUNT < total_rp_count:
+                    if 0 < GET_PAGE_COUNT < total_album_count:
                         is_over = True
                         break
                     else:
-                        total_rp_count += 1
+                        total_album_count += 1
 
                 if not is_over:
-                    if page_count >= get_max_page_count(coser_id, post_page_response.data):
+                    if page_count >= get_max_page_count(account_id, post_page_response.data):
                         is_over = True
                     else:
                         page_count += 1
 
-            log.step(cn + " 下载完毕，总共获得%s张图片" % this_cn_total_image_count)
+            log.step(account_name + " 下载完毕，总共获得%s张图片" % this_account_total_image_count)
 
             # 新的存档记录
-            if first_rp_id != "":
-                self.account_info[1] = first_rp_id
+            if first_album_id != "":
+                self.account_info[1] = first_album_id
 
             # 保存最后的信息
             tool.write_file("\t".join(self.account_info), NEW_SAVE_DATA_PATH)
             self.thread_lock.acquire()
-            TOTAL_IMAGE_COUNT += this_cn_total_image_count
-            ACCOUNTS.remove(coser_id)
+            TOTAL_IMAGE_COUNT += this_account_total_image_count
+            ACCOUNTS.remove(account_id)
             self.thread_lock.release()
 
-            log.step(cn + " 完成")
+            log.step(account_name + " 完成")
         except SystemExit:
-            log.error(cn + " 异常退出")
+            log.error(account_name + " 异常退出")
         except Exception, e:
-            log.error(cn + " 未知异常")
+            log.error(account_name + " 未知异常")
             log.error(str(e) + "\n" + str(traceback.format_exc()))
 
 
