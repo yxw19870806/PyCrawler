392f6f7fd6b766a151075c2d75af49ad7488d93e
hikaru <hikaru870806@hotmail.com>
2017-01-26 13:57:37 +0800

    backup urllib2 version

---
+++ backup_urllib2/tuchong.py
@@ -0,0 +1,261 @@
+# -*- coding:UTF-8  -*-
+"""
+图虫图片爬虫
+https://tuchong.com/
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+from common import log, robot, tool
+import json
+import os
+import threading
+import time
+import traceback
+
+ACCOUNTS = []
+IMAGE_COUNT_PER_PAGE = 20  # 每次请求获取的图片数量
+TOTAL_IMAGE_COUNT = 0
+GET_PAGE_COUNT = 0
+IMAGE_TEMP_PATH = ""
+IMAGE_DOWNLOAD_PATH = ""
+NEW_SAVE_DATA_PATH = ""
+IS_SORT = True
+IS_DOWNLOAD_IMAGE = True
+
+
+# 获取账号的site_id（字母账号->数字账号)
+def get_site_id(account_name):
+    index_url = "https://%s.tuchong.com" % account_name
+    index_return_code, index_page = tool.http_request(index_url)[:2]
+    if index_return_code == 1:
+        return tool.find_sub_string(index_page, '<div class="time-line" data-site-id="', '">')
+    return None
+
+
+# 根据指定时间点起的一页相册信息列表
+# account_name -> deer-vision
+# account_id -> 1186455
+# post_time -> 2016-11-11 11:11:11
+def get_one_page_post_info_list(site_id, post_time):
+    # https://deer-vision.tuchong.com/rest/sites/1186455/posts/2016-11-11%2011:11:11?limit=20
+    post_page_url = "https://www.tuchong.com/rest/sites/%s/posts/" % site_id
+    post_page_url += "%s?limit=%s" % (post_time, IMAGE_COUNT_PER_PAGE)
+    post_page_return_code, post_page_data = tool.http_request(post_page_url)[:2]
+    if post_page_return_code == 1:
+        try:
+            post_page_data = json.loads(post_page_data)
+        except ValueError:
+            pass
+        else:
+            if robot.check_sub_key(("posts", "result"), post_page_data) and post_page_data["result"] == "SUCCESS":
+                return post_page_data["posts"]
+    return None
+
+
+# 生成图片大图下载地址
+def generate_large_image_url(site_id, image_id):
+    return "https://photo.tuchong.com/%s/f/%s.jpg" % (site_id, image_id)
+
+
+class TuChong(robot.Robot):
+    def __init__(self):
+        global GET_PAGE_COUNT
+        global IMAGE_TEMP_PATH
+        global IMAGE_DOWNLOAD_PATH
+        global NEW_SAVE_DATA_PATH
+        global IS_SORT
+        global IS_DOWNLOAD_IMAGE
+
+        sys_config = {
+            robot.SYS_DOWNLOAD_IMAGE: True,
+        }
+        robot.Robot.__init__(self, sys_config)
+
+        # 设置全局变量，供子线程调用
+        GET_PAGE_COUNT = self.get_page_count
+        IMAGE_TEMP_PATH = self.image_temp_path
+        IMAGE_DOWNLOAD_PATH = self.image_download_path
+        IS_SORT = self.is_sort
+        IS_DOWNLOAD_IMAGE = self.is_download_image
+        NEW_SAVE_DATA_PATH = robot.get_new_save_file_path(self.save_data_path)
+
+    def main(self):
+        global ACCOUNTS
+
+        # 解析存档文件
+        # account_id  last_post_id
+        account_list = robot.read_save_data(self.save_data_path, 0, ["", "0"])
+        ACCOUNTS = account_list.keys()
+
+        # 循环下载每个id
+        main_thread_count = threading.activeCount()
+        for account_id in sorted(account_list.keys()):
+            # 检查正在运行的线程数
+            while threading.activeCount() >= self.thread_count + main_thread_count:
+                if robot.is_process_end() == 0:
+                    time.sleep(10)
+                else:
+                    break
+
+            # 提前结束
+            if robot.is_process_end() > 0:
+                break
+
+            # 开始下载
+            thread = Download(account_list[account_id], self.thread_lock)
+            thread.start()
+
+            time.sleep(1)
+
+        # 检查除主线程外的其他所有线程是不是全部结束了
+        while threading.activeCount() > main_thread_count:
+            time.sleep(10)
+
+        # 未完成的数据保存
+        if len(ACCOUNTS) > 0:
+            new_save_data_file = open(NEW_SAVE_DATA_PATH, "a")
+            for account_id in ACCOUNTS:
+                new_save_data_file.write("\t".join(account_list[account_id]) + "\n")
+            new_save_data_file.close()
+
+        # 删除临时文件夹
+        self.finish_task()
+
+        # 重新排序保存存档文件
+        robot.rewrite_save_file(NEW_SAVE_DATA_PATH, self.save_data_path)
+
+        log.step("全部下载完毕，耗时%s秒，共计图片%s张" % (self.get_run_time(), TOTAL_IMAGE_COUNT))
+
+
+class Download(threading.Thread):
+    def __init__(self, account_info, thread_lock):
+        threading.Thread.__init__(self)
+        self.account_info = account_info
+        self.thread_lock = thread_lock
+
+    def run(self):
+        global TOTAL_IMAGE_COUNT
+
+        account_name = self.account_info[0]
+
+        try:
+            log.step(account_name + " 开始")
+
+            if account_name.isdigit():
+                site_id = account_name
+            else:
+                site_id = get_site_id(account_name)
+            if site_id is None:
+                log.error(account_name + " 主页无法访问")
+                tool.process_exit()
+
+            if not site_id:
+                log.error(account_name + " site id解析失败")
+                tool.process_exit()
+
+            image_path = os.path.join(IMAGE_DOWNLOAD_PATH, account_name)
+
+            this_account_total_image_count = 0
+            post_count = 0
+            first_post_id = "0"
+            post_time = "2016-11-16 14:12:00"
+            is_over = False
+            while not is_over:
+                log.step(account_name + " 开始解析%s后的一页相册" % post_time)
+                
+                # 获取一页的相册信息列表
+                post_info_list = get_one_page_post_info_list(site_id, post_time)
+                if post_info_list is None:
+                    log.error(account_name + " 相册信息列表无法访问")
+                    tool.process_exit()
+
+                # 如果为空，表示已经取完了
+                if len(post_info_list) == 0:
+                    break
+
+                log.trace(account_name + " %s后获取的一页相册：%s" % (post_time, post_info_list))
+
+                for post_info in post_info_list:
+                    if not robot.check_sub_key(("title", "post_id", "published_at", "images"), post_info):
+                        log.error(account_name + " 相册信息解析失败：%s" % post_info)
+                        continue
+
+                    post_id = str(post_info["post_id"])
+
+                    # 检查信息页id是否小于上次的记录
+                    if int(post_id) <= int(self.account_info[1]):
+                        is_over = True
+                        break
+
+                    # 将第一个信息页的id做为新的存档记录
+                    if first_post_id == "0":
+                        first_post_id = post_id
+
+                    log.step(account_name + " 开始解析相册%s" % post_id)
+
+                    # 过滤标题中不支持的字符
+                    title = robot.filter_text(post_info["title"])
+                    if title:
+                        post_path = os.path.join(image_path, "%s %s" % (post_id, title))
+                    else:
+                        post_path = os.path.join(image_path, post_id)
+                    if not tool.make_dir(post_path, 0):
+                        # 目录出错，把title去掉后再试一次，如果还不行退出
+                        log.error(account_name + " 创建相册目录 %s 失败，尝试不使用title" % post_path)
+                        post_path = os.path.join(image_path, post_id)
+                        if not tool.make_dir(post_path, 0):
+                            log.error(account_name + " 创建相册目录 %s 失败" % post_path)
+                            tool.process_exit()
+
+                    image_count = 0
+                    for image_info in post_info["images"]:
+                        image_count += 1
+                        if not robot.check_sub_key(("img_id",), image_info):
+                            log.error(account_name + " 相册%s 第%s张图片解析失败" % (post_id, image_count))
+                            continue
+                        image_url = generate_large_image_url(site_id, image_info["img_id"])
+                        log.step(account_name + " 相册%s 开始下载第%s张图片 %s" % (post_id, image_count, image_url))
+
+                        file_path = os.path.join(post_path, "%s.jpg" % image_count)
+                        if tool.save_net_file(image_url, file_path):
+                            log.step(account_name + " 相册%s 第%s张图片下载成功" % (post_id, image_count))
+                        else:
+                            log.error(account_name + " 相册%s 第%s张图片 %s 下载失败" % (post_info["post_id"], image_count, image_url))
+                    this_account_total_image_count += image_count
+
+                    if not is_over:
+                        # 达到配置文件中的下载页数，结束
+                        if 0 < GET_PAGE_COUNT < post_count:
+                            is_over = True
+                        else:
+                            # 相册发布时间
+                            post_time = post_info["published_at"]
+                            post_count += 1
+
+            log.step(account_name + " 下载完毕，总共获得%s张图片" % this_account_total_image_count)
+
+            # 新的存档记录
+            if first_post_id != "0":
+                self.account_info[1] = first_post_id
+
+            # 保存最后的信息
+            tool.write_file("\t".join(self.account_info), NEW_SAVE_DATA_PATH)
+            self.thread_lock.acquire()
+            TOTAL_IMAGE_COUNT += this_account_total_image_count
+            ACCOUNTS.remove(account_name)
+            self.thread_lock.release()
+
+            log.step(account_name + " 完成")
+        except SystemExit, se:
+            if se.code == 0:
+                log.step(account_name + " 提前退出")
+            else:
+                log.error(account_name + " 异常退出")
+        except Exception, e:
+            log.error(account_name + " 未知异常")
+            log.error(str(e) + "\n" + str(traceback.format_exc()))
+
+
+if __name__ == "__main__":
+    TuChong().main()
---
+++ backup_urllib2/zunguang.py
@@ -0,0 +1,146 @@
+# -*- coding:UTF-8  -*-
+"""
+尊光图片爬虫
+http://zunguang.com/
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+from common import log, robot, tool
+import json
+import os
+
+ERROR_PAGE_COUNT_CHECK = 10
+
+
+# 根据页面内容获取图片地址列表
+def get_one_page_album_data(page_count):
+    album_url = "http://www.zunguang.com/index.php?c=api&yc=blog&ym=getOneBlog"
+    post_data = {"bid": page_count}
+    album_return_code, album_data = tool.http_request(album_url, post_data, is_random_ip=False)[:2]
+    if album_return_code != 1:
+        return -1, None
+    try:
+        album_data = json.loads(album_data)
+    except ValueError:
+        return -2, None  # JSON decode error
+    if robot.check_sub_key(("body",), album_data) and robot.check_sub_key(("blog",), album_data["body"]):
+        if not album_data["body"]["blog"]:
+            return 2, None  # 相册已被已被删除
+        blog_type = int(album_data["body"]["blog"][0]["type"])
+        if blog_type == 2:
+            return 3, None  # 歌曲类型的相册
+        elif blog_type == 3:
+            album_body = album_data["body"]["blog"][0]
+            if robot.check_sub_key(("title", "attr"), album_body) and robot.check_sub_key(("img",), album_body["attr"]):
+                return 1, album_body
+        else:
+            return 4, blog_type
+
+
+class ZunGuang(robot.Robot):
+    def __init__(self):
+        sys_config = {
+            robot.SYS_DOWNLOAD_IMAGE: True,
+            robot.SYS_NOT_CHECK_SAVE_DATA: True,
+        }
+        robot.Robot.__init__(self, sys_config)
+
+        tool.print_msg("配置文件读取完成")
+
+    def main(self):
+        # 解析存档文件，获取上一次的album id
+        page_count = 1
+        if os.path.exists(self.save_data_path):
+            save_file = open(self.save_data_path, "r")
+            save_info = save_file.read()
+            save_file.close()
+            page_count = int(save_info.strip())
+
+        total_image_count = 0
+        error_count = 0
+        is_over = False
+        while not is_over:
+            log.step("开始解析第%s页图片" % page_count)
+
+            # 根据取图片地址列表
+            album_status, album_data = get_one_page_album_data(page_count)
+            if album_status == -1:
+                log.error("第%s页相册获取失败" % page_count)
+                break
+            elif album_status == -2:
+                log.error("第%s页相册解析失败" % page_count)
+                break
+            elif album_status == 2:
+                error_count += 1
+                if error_count >= ERROR_PAGE_COUNT_CHECK:
+                    log.error("连续%s页相册没有图片，退出程序" % ERROR_PAGE_COUNT_CHECK)
+                    page_count -= error_count - 1
+                    break
+                else:
+                    log.error("第%s页相册已被删除" % page_count)
+                    page_count += 1
+                    continue
+            elif album_status == 3:
+                log.error("第%s页歌曲相册" % page_count)
+                page_count += 1
+                continue
+            elif album_status == 4:
+                log.error("第%s页相册未知相册类型%s" % (page_count, album_data))
+                break
+            # 错误数量重置
+            error_count = 0
+
+            # 下载目录标题
+            title = ""
+            if album_data["title"]:
+                # 过滤标题中不支持的字符
+                title = robot.filter_text(str(album_data["title"].encode("utf-8")))
+            if title:
+                image_path = os.path.join(self.image_download_path, "%04d %s" % (page_count, title))
+            else:
+                image_path = os.path.join(self.image_download_path, "%04d" % page_count)
+            if not tool.make_dir(image_path, 0):
+                # 目录出错，把title去掉后再试一次，如果还不行退出
+                log.error("第%s页创建相册目录 %s 失败，尝试不使用title" % (page_count, image_path))
+                post_path = os.path.join(image_path, page_count)
+                if not tool.make_dir(post_path, 0):
+                    log.error("第%s页创建相册目录 %s 失败" % (page_count, image_path))
+                    tool.process_exit()
+
+            image_count = 1
+            for image_data in album_data["attr"]["img"]:
+                image_url = "http://www.zunguang.com/%s" % str(image_data["url"])
+                log.step("开始下载第%s页第%s张图片 %s" % (page_count, image_count, image_url))
+
+                file_type = image_url.split(".")[-1]
+                file_path = os.path.join(image_path, "%03d.%s" % (image_count, file_type))
+                try:
+                    if tool.save_net_file(image_url, file_path, True):
+                        log.step("第%s页第%s张图片下载成功" % (page_count, image_count))
+                        image_count += 1
+                    else:
+                        log.error("第%s页第%s张图片 %s 下载失败" % (page_count, image_count, image_url))
+                except SystemExit:
+                    log.step("提前退出")
+                    tool.remove_dir(image_path)
+                    is_over = True
+                    break
+
+            if not is_over:
+                total_image_count += image_count - 1
+                page_count += 1
+
+        # 重新保存存档文件
+        save_data_dir = os.path.dirname(self.save_data_path)
+        if not os.path.exists(save_data_dir):
+            tool.make_dir(save_data_dir, 0)
+        save_file = open(self.save_data_path, "w")
+        save_file.write(str(page_count))
+        save_file.close()
+
+        log.step("全部下载完毕，耗时%s秒，共计图片%s张" % (self.get_run_time(), total_image_count))
+
+
+if __name__ == "__main__":
+    ZunGuang().main()
