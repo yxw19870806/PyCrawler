99966149263cdb6d9173c4dc373f2d9c41de2d45
hikaru <hikaru870806@hotmail.com>
2017-01-26 13:58:05 +0800

    urllib3 parm

--- flickr/flickr.py
+++ flickr/flickr.py
@@ -99,7 +99,7 @@ class Flickr(robot.Robot):
             robot.SYS_DOWNLOAD_IMAGE: True,
             robot.SYS_SET_PROXY: True,
         }
-        robot.Robot.__init__(self, sys_config)
+        robot.Robot.__init__(self, sys_config, use_urllib3=True)
 
         # 设置全局变量，供子线程调用
         GET_IMAGE_COUNT = self.get_image_count
