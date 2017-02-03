5fc8af835ef566da63967ca7f01f43b7a7744519
hikaru <hikaru870806@hotmail.com>
2017-02-03 16:08:06 +0800

    增加参数，是否对post数据使用multipart/form-data方式进行传送

--- common/net.py
+++ common/net.py
@@ -60,7 +60,7 @@ def set_proxy(ip, port):
 #                   -2：json decode error
 #                   -10：特殊异常捕获后的返回
 #                   其他>0：网页返回码（正常返回码为200）
-def http_request(url, post_data=None, header_list=None, is_random_ip=True, json_decode=False, exception_return=""):
+def http_request(url, post_data=None, header_list=None, is_random_ip=True, json_decode=False, encode_multipart=False, exception_return=""):
     if not (url.find("http://") == 0 or url.find("https://") == 0):
         return ErrorResponse(HTTP_RETURN_CODE_URL_INVALID)
     if HTTP_CONNECTION_POOL is None:
@@ -87,7 +87,7 @@ def http_request(url, post_data=None, header_list=None, is_random_ip=True, json_
 
         try:
             if post_data:
-                response = HTTP_CONNECTION_POOL.request('POST', url, fields=post_data, headers=header_list)
+                response = HTTP_CONNECTION_POOL.request('POST', url, fields=post_data, headers=header_list, encode_multipart=encode_multipart)
             else:
                 response = HTTP_CONNECTION_POOL.request('GET', url, headers=header_list)
             if json_decode:
