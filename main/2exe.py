aab70311d24202ed702cfc7611190a67b870a2b0
hikaru <hikaru870806@hotmail.com>
2017-03-16 20:05:56 +0800

    增加是否需要压缩的参数&注释

--- main/2exe.py
+++ main/2exe.py
@@ -12,7 +12,7 @@ import py2exe
 import shutil
 
 
-def create_exe(py_file_path, need_zip=False):
+def create_exe(py_file_path, need_config=False, need_zip=False):
     build_path = os.path.realpath(".\\build")
     build_dist_path = os.path.realpath(".\\dist")
     py_file_name = ".".join(os.path.basename(py_file_path).split(".")[:-1])
@@ -24,15 +24,20 @@ def create_exe(py_file_path, need_zip=False):
     # 打包
     setup(console=[py_file_path])
 
-    # 删除临时文件 + 复制其他必要文件
-    tool.make_dir(os.path.join(build_dist_path, "data\\"), 0)
-    tool.copy_files(os.path.realpath("..\\common\\config_exe.ini"), os.path.join(build_dist_path, "data\\config.ini"))
+    # 复制其他必要文件
+    if need_config:
+        tool.make_dir(os.path.join(build_dist_path, "data\\"), 0)
+        tool.copy_files(os.path.realpath("..\\common\\config_exe.ini"), os.path.join(build_dist_path, "data\\config.ini"))
+
+    # 删除临时目录
+    tool.remove_dir(build_path)
+
+    # 是否需要压缩
     if need_zip:
         extraTool.zip_dir(build_dist_path, os.path.realpath("%s.zip" % py_file_name))
         tool.remove_dir(build_dist_path)
     else:
         shutil.move(build_dist_path, os.path.realpath(".\\%s" % py_file_name))
-    tool.remove_dir(build_path)
 
 
 if __name__ == "__main__":
