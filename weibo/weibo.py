14b4bdd80b467a94eeadb05a29e89db6e52301cf
hikaru <hikaru870806@hotmail.com>
2017-03-20 15:23:50 +0800

    图片下载部分更新为urllib3 版本

--- weibo/weibo.py
+++ weibo/weibo.py
@@ -6,7 +6,7 @@ http://www.weibo.com/
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
-from common import log, robot, tool
+from common import log, net, robot, tool
 from common import net_tool
 import hashlib
 import json
@@ -33,6 +33,7 @@ NEW_SAVE_DATA_PATH = ""
 IS_SORT = True
 IS_DOWNLOAD_IMAGE = True
 IS_DOWNLOAD_VIDEO = True
+COOKIE_INFO = {"SUB": ""}
 
 
 # 图片二进制字节保存为本地文件
@@ -69,19 +70,43 @@ def auto_redirect_visit(url):
 
 
 # 获取一页的图片信息
-def get_one_page_photo_data(account_id, page_count):
-    photo_page_url = "http://photo.weibo.com/photos/get_all"
-    photo_page_url += "?uid=%s&count=%s&page=%s&type=3" % (account_id, IMAGE_COUNT_PER_PAGE, page_count)
-    photo_page_data = auto_redirect_visit(photo_page_url)
-    try:
-        page = json.loads(photo_page_data)
-    except ValueError:
-        pass
-    else:
-        if robot.check_sub_key(("data",), page):
-            if robot.check_sub_key(("total", "photo_list"), page["data"]):
-                return page["data"]
-    return None
+def get_one_page_photo(account_id, page_count):
+    index_page_url = "http://photo.weibo.com/photos/get_all?uid=%s&count=%s&page=%s&type=3" % (account_id, IMAGE_COUNT_PER_PAGE, page_count)
+    header_list = {"cookie": "SUB=" + COOKIE_INFO["SUB"]}
+    extra_info = {
+        "is_error": True,  # 是不是格式不符合
+        "image_info_list": [],  # 页面解析出的图片信息列表
+        "is_over": False,  # 是不是最后一页图片
+    }
+    index_page_response = net.http_request(index_page_url, header_list=header_list, json_decode=True)
+    if index_page_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        if (
+            robot.check_sub_key(("data",), index_page_response.json_data) and
+            robot.check_sub_key(("total", "photo_list"), index_page_response.json_data["data"]) and
+            robot.is_integer(index_page_response.json_data["data"]["total"])
+        ):
+            extra_info["is_error"] = False
+            for image_info in index_page_response.json_data["data"]["photo_list"]:
+                extra_image_info = {
+                    "image_time": None,  # 页面解析出的图片上传时间
+                    "image_url": None,  # 页面解析出的图片地址
+                    "json_data": image_info,  # 原始数据
+                }
+                if robot.check_sub_key(("timestamp",), image_info) and robot.is_integer(image_info["timestamp"]):
+                    extra_image_info["image_time"] = int(image_info["timestamp"])
+                else:
+                    extra_info["is_error"] = True
+                    break
+                if robot.check_sub_key(("pic_host", "pic_name"), image_info):
+                    extra_image_info["image_url"] = str(image_info["pic_host"]) + "/large/" + str(image_info["pic_name"])
+                else:
+                    extra_info["is_error"] = True
+                    break
+                extra_info["image_info_list"].append(extra_image_info)
+            # 检测是不是还有下一页 总的图片数量 / 每页显示的图片数量 = 总的页数
+            extra_info["is_over"] = page_count >= (index_page_response.json_data["data"]["total"] * 1.0 / IMAGE_COUNT_PER_PAGE)
+    index_page_response.extra_info = extra_info
+    return index_page_response
 
 
 # 获取账号对应的page_id
@@ -205,20 +230,12 @@ def get_video_url(video_play_url):
         return -3, None
 
 
-# 访问图片源地址，判断是不是图片已经被删除或暂时无法访问后，返回图片字节
-def get_image_byte(image_url):
-    for i in range(0, 10):
-        image_return_code, image_data = net_tool.http_request(image_url)[:2]
-        if image_return_code == 1:
-            # 处理获取的文件为weibo默认获取失败的图片
-            md5_digest = md5(image_data)
-            if md5_digest in ["14f2559305a6c96608c474f4ca47e6b0", "37b9e6dec174b68a545c852c63d4645a"]:
-                return -2, None  # 被系统自动删除的图片
-            # 不是暂时无法访问，否则重试
-            if md5_digest not in ["d29352f3e0f276baaf97740d170467d7", "7bd88df2b5be33e1a79ac91e7d0376b5"]:
-                return 1, image_data
-        time.sleep(5)
-    return -1, None
+# 检测图片是不是被微博自动删除的文件
+def check_image_invalid(file_path):
+    file_md5 = tool.get_file_md5(file_path)
+    if file_md5 in ["14f2559305a6c96608c474f4ca47e6b0", "37b9e6dec174b68a545c852c63d4645a"]:
+        return True
+    return False
 
 
 class Weibo(robot.Robot):
@@ -233,11 +250,13 @@ class Weibo(robot.Robot):
         global IS_SORT
         global IS_DOWNLOAD_IMAGE
         global IS_DOWNLOAD_VIDEO
+        global COOKIE_INFO
 
         sys_config = {
             robot.SYS_DOWNLOAD_IMAGE: True,
-            robot.SYS_DOWNLOAD_VIDEO: True,
+            # robot.SYS_DOWNLOAD_VIDEO: True,
             robot.SYS_SET_COOKIE: ("weibo.com", ".sina.com.cn"),
+            robot.SYS_GET_COOKIE: {".sina.com.cn": ("SUB",)},
         }
         robot.Robot.__init__(self, sys_config, extra_config)
 
@@ -252,6 +271,7 @@ class Weibo(robot.Robot):
         IS_DOWNLOAD_IMAGE = self.is_download_image
         IS_DOWNLOAD_VIDEO = self.is_download_video
         NEW_SAVE_DATA_PATH = robot.get_new_save_file_path(self.save_data_path)
+        COOKIE_INFO["SUB"] = self.cookie_value["SUB"]
 
     def main(self):
         global ACCOUNTS
@@ -262,8 +282,8 @@ class Weibo(robot.Robot):
         ACCOUNTS = account_list.keys()
 
         # 先访问下页面，产生cookies
-        auto_redirect_visit("http://www.weibo.com/")
-        time.sleep(2)
+        # auto_redirect_visit("http://www.weibo.com/")
+        # time.sleep(2)
 
         # 循环下载每个id
         main_thread_count = threading.activeCount()
@@ -423,45 +443,38 @@ class Download(threading.Thread):
                 log.step(account_name + " 开始解析第%s页图片" % page_count)
 
                 # 获取指定一页图片的信息
-                photo_page_data = get_one_page_photo_data(account_id, page_count)
-                if photo_page_data is None:
-                    log.error(account_name + " 图片列表解析失败")
-                    first_image_time = "0"  # 存档恢复
-                    break
+                index_page_response = get_one_page_photo(account_id, page_count)
+                if index_page_response.status != net.HTTP_RETURN_CODE_SUCCEED:
+                    log.error(account_name + " 第%s页图片访问失败，原因：%s" % (page_count, robot.get_http_request_failed_reason(index_page_response.status)))
+                    tool.process_exit()
 
-                log.trace(account_name + "第%s页解析的全部图片信息：%s" % (page_count, photo_page_data))
+                if index_page_response.extra_info["is_error"]:
+                    log.error(account_name + " 第%s页图片%s解析失败" % (page_count, index_page_response.json_data))
+                    tool.process_exit()
 
-                for image_info in photo_page_data["photo_list"]:
-                    if not robot.check_sub_key(("pic_host", "pic_name", "timestamp"), image_info):
-                        log.error(account_name + " 第%s张图片信息解析失败 %s" % (image_count, image_info))
-                        continue
+                log.trace(account_name + "第%s页解析的全部图片信息：%s" % (page_count, index_page_response.extra_info["image_info_list"]))
+
+                for image_info in index_page_response.extra_info["image_info_list"]:
+                    if image_info["image_time"] is None:
+                        log.error(account_name + " 第%s页图片%s解析失败" % (page_count, index_page_response.json_data))
+                        tool.process_exit()
 
                     # 检查是否图片时间小于上次的记录
-                    if int(image_info["timestamp"]) <= int(self.account_info[2]):
+                    if image_info["image_time"] <= int(self.account_info[2]):
                         is_over = True
                         break
 
                     # 将第一张图片的上传时间做为新的存档记录
                     if first_image_time == "0":
-                        first_image_time = str(image_info["timestamp"])
+                        first_image_time = str(image_info["image_time"])
 
                     # 新增图片导致的重复判断
-                    if image_info["pic_name"] in unique_list:
+                    if image_info["image_url"] in unique_list:
                         continue
                     else:
-                        unique_list.append(image_info["pic_name"])
+                        unique_list.append(image_info["image_url"])
 
-                    image_url = str(image_info["pic_host"]) + "/large/" + str(image_info["pic_name"])
-                    log.step(account_name + " 开始下载第%s张图片 %s" % (image_count, image_url))
-
-                    # 获取图片的二进制数据，并且判断这个图片是否是可用的
-                    image_status, image_byte = get_image_byte(image_url)
-                    if image_status != 1:
-                        if image_status == -1:
-                            log.error(account_name + " 第%s张图片 %s 下载失败" % (image_count, image_url))
-                        elif image_status == -2:
-                            log.error(account_name + " 第%s张图片 %s 资源已被删除，跳过" % (image_count, image_url))
-                        continue
+                    log.step(account_name + " 开始下载第%s张图片 %s" % (image_count, image_info["image_url"]))
 
                     # 第一张图片，创建目录
                     if need_make_image_dir:
@@ -470,13 +483,19 @@ class Download(threading.Thread):
                             tool.process_exit()
                         need_make_image_dir = False
 
-                    file_type = image_url.split(".")[-1]
+                    file_type = image_info["image_url"].split(".")[-1]
                     if file_type.find("/") != -1:
                         file_type = "jpg"
                     image_file_path = os.path.join(image_path, "%04d.%s" % (image_count, file_type))
-                    save_image(image_byte, image_file_path)
-                    log.step(account_name + " 第%s张图片下载成功" % image_count)
-                    image_count += 1
+                    save_return = net.save_net_file(image_info["image_url"], image_file_path)
+                    if save_return["status"] == 1:
+                        if check_image_invalid(image_file_path):
+                            log.error(account_name + " 第%s张图片 %s 资源已被删除，跳过" % (image_count, image_info["image_url"]))
+                        else:
+                            log.step(account_name + " 第%s张图片下载成功" % image_count)
+                            image_count += 1
+                    else:
+                        log.error(account_name + " 第%s张图片 %s 下载失败，原因：%s" % (image_count, image_info["image_url"], robot.get_save_net_file_failed_reason(save_return["code"])))
 
                     # 达到配置文件中的下载数量，结束
                     if 0 < GET_IMAGE_COUNT < image_count:
@@ -484,12 +503,10 @@ class Download(threading.Thread):
                         break
 
                 if not is_over:
-                    # 根据总的图片数量和每页显示的图片数量，计算是否还有下一页
-                    if (photo_page_data["total"] / IMAGE_COUNT_PER_PAGE) > (page_count - 1):
-                        page_count += 1
-                    else:
-                        # 全部图片下载完毕
+                    if index_page_response.extra_info["is_over"]:
                         is_over = True
+                    else:
+                        page_count += 1
 
             log.step(account_name + " 下载完毕，总共获得%s张图片和%s个视频" % (image_count - 1, video_count - 1))
 
