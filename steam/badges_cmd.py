733f4d38aba929d23f0b7c5b756b939f0bbeb026
hikaru <hikaru870806@hotmail.com>
2017-04-27 13:14:53 +0800

    增加一个直接从命令行运行的入口

---
+++ steam/badges_cmd.py
@@ -0,0 +1,21 @@
+# -*- coding:UTF-8  -*-
+"""
+命令行入口文件，非IDE执行
+@author: hikaru
+email: hikaru870806@hotmail.com
+如有问题或建议请联系
+"""
+import os
+import sys
+root_path = os.path.realpath(os.path.join(os.path.dirname(sys._getframe().f_code.co_filename), ".."))
+if root_path not in sys.path:
+    sys.path.append(root_path)
+os.chdir(root_path)
+
+
+import steamCommon
+import badges
+
+
+account_id = steamCommon.get_account_id_from_file()
+badges.main(account_id)
