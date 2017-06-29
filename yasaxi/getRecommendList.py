e9a16391692cfd29931b95c2df84d1f114a95bb8
hikaru <hikaru870806@hotmail.com>
2017-06-29 18:35:56 +0800

    从文件中读取账号token

--- yasaxi/getRecommendList.py
+++ yasaxi/getRecommendList.py
@@ -6,10 +6,37 @@ email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 from common import *
+import base64
+import json
+import os
 
 
 AUTH_TOKEN = ""
 ZHEZHE_INFO = ""
+
+
+# 从文件中获取用户信息
+def get_account_info_from_file():
+    account_file_path = os.path.realpath("account.data")
+    if not os.path.exists(account_file_path):
+        return False
+    file_handle = open(account_file_path, "r")
+    file_string = file_handle.read()
+    file_handle.close()
+    file_string.replace("\n", "")
+    try:
+        account_data = json.loads(base64.b64decode(file_string))
+    except TypeError:
+        return False
+    except ValueError:
+        return False
+    if robot.check_sub_key(("access_token", "auth_token", "zhezhe_info"), account_data):
+        global AUTH_TOKEN
+        global ZHEZHE_INFO
+        AUTH_TOKEN = account_data["auth_token"]
+        ZHEZHE_INFO = account_data["zhezhe_info"]
+        return True
+    return False
 
 
 def get_recommend():
@@ -25,4 +52,4 @@ def get_recommend():
                 print "%s\t%s" % (str(account_info["userId"].encode("utf-8")), robot.filter_emoji(str(account_info["nick"].encode("utf-8")).strip()))
 
 if __name__ == "__main__":
-    get_recommend()
+    get_account_info_from_file() and get_recommend()
