8bee179ba9ac35b2f738bfee3ecf4cda4d1f88ba
hikaru <hikaru870806@hotmail.com>
2017-07-04 18:34:53 +0800

    增加是否只显示部分类型的游戏，如game、package、bundle

--- steam/discount.py
+++ steam/discount.py
@@ -11,6 +11,11 @@ import json
 import os
 
 
+INCLUDE_GAME = True
+INCLUDE_PACKAGE = True
+INCLUDE_BUNDLE = True
+
+
 # 打折游戏列表保存到文件
 def save_discount_list(discount_game_list):
     tool.write_file(json.dumps(discount_game_list), "discount.txt", 2)
@@ -36,7 +41,7 @@ def load_discount_list():
 # 给出给定大等于最低折扣或者小等于最低价格的所有还没有的打折游戏
 # min_discount_percent  最低折扣
 # min_discount_price    最低价格
-def main(account_id, min_discount_percent, min_discount_price):
+def main(account_id, include_type, min_discount_percent, min_discount_price):
     login_cookie = steamCommon.get_login_cookie_from_browser()
     discount_game_list = load_discount_list()
     if not discount_game_list:
@@ -50,6 +55,10 @@ def main(account_id, min_discount_percent, min_discount_price):
         if discount_info["now_price"] > 0 and discount_info["old_price"] > 0 and (discount_info["now_price"] <= min_discount_price or discount_info["discount"] >= min_discount_percent):
             # bundle 或者 package，都包含多个游戏
             if discount_info["type"] == "bundle" or discount_info["type"] == "package":
+                if discount_info["type"] == "package" and include_type & 2 == 0:
+                    continue
+                if discount_info["type"] == "bundle" and include_type & 4 == 0:
+                    continue
                 is_all = True
                 # 遍历包含的全部游戏，如果都有了，则跳过
                 for app_id in discount_info["app_id"]:
@@ -62,9 +71,19 @@ def main(account_id, min_discount_percent, min_discount_price):
                     else:
                         tool.print_msg("http://store.steampowered.com/sub/%s ,discount %s%%, old price: %s, discount price: %s" % (discount_info["id"], discount_info["discount"], discount_info["old_price"], discount_info["now_price"]), False)
             else:
+                if include_type & 1 == 0:
+                    continue
                 if discount_info["app_id"] not in owned_game_list:
                     tool.print_msg("http://store.steampowered.com/app/%s/ ,discount %s%%, old price: %s, discount price: %s" % (discount_info["id"], discount_info["discount"], discount_info["old_price"], discount_info["now_price"]), False)
 
 
 if __name__ == "__main__":
-    main(steamCommon.get_account_id_from_file(), 90, 1)
+    include_type_id = 0
+    if INCLUDE_GAME:
+        include_type_id += 1
+    if INCLUDE_PACKAGE:
+        include_type_id += 2
+    if INCLUDE_BUNDLE:
+        include_type_id += 4
+
+    main(steamCommon.get_account_id_from_file(), include_type_id, 90, 1)
