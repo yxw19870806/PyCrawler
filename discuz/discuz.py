ffd14200894678f532170fec7891583b97ad1d64
hikaru <hikaru870806@hotmail.com>
2017-07-10 19:46:22 +0800

    update

--- discuz/discuz.py
+++ discuz/discuz.py
@@ -10,11 +10,11 @@ import re
 
 
 # 获取论坛所有版块的地址列表
-def get_bbs_forum_url_list(index_page_url):
-    index_page_response = net.http_request(index_page_url)
-    if index_page_response.status == net.HTTP_RETURN_CODE_SUCCEED:
-        forum_find = re.findall('<a href="(forum-\w*-\d*.\w*)"[^>]*>([\S]*)</a>', index_page_response.data)
-        host = index_page_url[0: index_page_url.rfind("/") + 1]
+def get_bbs_forum_url_list(index_url):
+    index_response = net.http_request(index_url)
+    if index_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        forum_find = re.findall('<a href="(forum-\w*-\d*.\w*)"[^>]*>([\S]*)</a>', index_response.data)
+        host = index_url[0: index_url.rfind("/") + 1]
         forum_url_list = {}
         for forum_path, forum_name in forum_find:
             forum_url_list[host + forum_path] = forum_name
@@ -23,12 +23,12 @@ def get_bbs_forum_url_list(index_page_url):
 
 
 # 获取论坛板块一页的帖子地址列表
-def get_one_forum_page_thread_url_list(forum_page_url):
-    forum_page_response = net.http_request(forum_page_url)
-    if forum_page_response.status == net.HTTP_RETURN_CODE_SUCCEED:
-        forum_page = tool.find_sub_string(forum_page_response.data, '<div id="threadlist"', '<div id="filter_special_menu"', 1)
+def get_one_forum_page_thread_url_list(forum_url):
+    forum_response = net.http_request(forum_url)
+    if forum_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        forum_page = tool.find_sub_string(forum_response.data, '<div id="threadlist"', '<div id="filter_special_menu"', 1)
         thread_find = re.findall('<a href="(thread-\d*-1-1.\w*)" onclick="atarget\(this\)" class="s xst">([\S|\s]*?)</a>', forum_page)
-        host = forum_page_url[0: forum_page_url.rfind("/") + 1]
+        host = forum_url[0: forum_url.rfind("/") + 1]
         thread_url_list = {}
         for forum_path, forum_name in thread_find:
             thread_url_list[host + forum_path] = forum_name
@@ -37,11 +37,11 @@ def get_one_forum_page_thread_url_list(forum_page_url):
 
 
 # 获取帖子作者楼层内容
-def get_thread_author_post(thread_page_url):
-    thread_page_response = net.http_request(thread_page_url)
-    if thread_page_response.status == net.HTTP_RETURN_CODE_SUCCEED:
-        charset = tool.find_sub_string(thread_page_response.headers["Content-Type"], "charset=")
-        post_message = tool.find_sub_string(thread_page_response.data, '<td class="t_f" id="postmessage_', '<div id="comment_')
+def get_thread_author_post(thread_url):
+    thread_response = net.http_request(thread_url)
+    if thread_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        charset = tool.find_sub_string(thread_response.headers["Content-Type"], "charset=")
+        post_message = tool.find_sub_string(thread_response.data, '<td class="t_f" id="postmessage_', '<div id="comment_')
         post_message = post_message[post_message.find('">') + 2: post_message.rfind("</td>")]
         return post_message.decode(charset)
     return None
