40ddebbc571b99793e30bbf214e74e78fa08e407
hikaru <hikaru870806@hotmail.com>
2017-08-04 16:26:46 +0800

    main方法中处理的格式不正确逻辑 改为 解析方法抛出异常

--- project/88mm/rosi.py
+++ project/88mm/rosi.py
@@ -20,21 +20,30 @@ def get_one_page_album(page_count):
         "album_info_list": [],  # 页面解析出的所有图集信息列表
     }
     if album_pagination_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        album_pagination_html = album_pagination_response.data.decode("GBK").encode("UTF-8")
+
         # 获取图集图片地址
-        album_info_html = tool.find_sub_string(album_pagination_response.data.decode("GBK").encode("UTF-8"), '<div class="xxx">', "</div>")
+        album_info_html = tool.find_sub_string(album_pagination_html, '<div class="xxx">', "</div>")
+        if not album_info_html:
+            raise robot.RobotException("页面截取图集列表失败\n%s" % album_pagination_html)
         album_info_list = re.findall('<a href="/Rosi/(\d*)/" title="ROSI套图No.(\d*)', album_info_html)
+        if len(album_info_list) == 0:
+            raise robot.RobotException("页面获取图集信息失败\n%s" % album_info_html)
         for page_id, album_id in album_info_list:
             extra_album_info = {
                 "page_id": str(page_id),  # 页面解析出的图集地址页面id
                 "album_id": str(album_id),  # 页面解析出的图集id
             }
             extra_info["album_info_list"].append(extra_album_info)
+
         # 判断是不是最后一页
-        max_page_find = re.findall("<a href='list_1_(\d*).html'>末页</a>", album_pagination_response.data.decode("GBK").encode("UTF-8"))
+        max_page_find = re.findall("<a href='list_1_(\d*).html'>末页</a>", album_pagination_html)
         if len(max_page_find) == 2 and max_page_find[0] == max_page_find[1] and robot.is_integer(max_page_find[0]):
             extra_info['is_over'] = page_count >= int(max_page_find[0])
         else:
             extra_info['is_over'] = True
+    else:
+        raise robot.RobotException(robot.get_http_request_failed_reason(album_pagination_response.status))
     album_pagination_response.extra_info = extra_info
     return album_pagination_response
 
@@ -51,16 +60,26 @@ def get_one_page_photo(page_id, page_count):
         "image_url_list": [],  # 页面解析出的所有图片地址列表
     }
     if photo_pagination_response.status == net.HTTP_RETURN_CODE_SUCCEED:
+        photo_pagination_html = photo_pagination_response.data.decode("GBK").encode("UTF-8")
+
         # 获取图片地址
-        image_url_list = re.findall('<img src="([^"]*)"', tool.find_sub_string(photo_pagination_response.data, '<div class="zzz">', "</div>"))
+        image_info_html = tool.find_sub_string(photo_pagination_html, '<div class="zzz">', "</div>")
+        if not image_info_html:
+            raise robot.RobotException("页面截取图片列表失败\n%s" % photo_pagination_html)
+        image_url_list = re.findall('<img src="([^"]*)"', image_info_html)
+        if len(image_url_list) == 0:
+            raise robot.RobotException("页面获取图片地址失败\n%s" % image_info_html)
         for image_url in image_url_list:
             extra_info["image_url_list"].append("http://www.88mmw.com" + str(image_url).replace("-lp", ""))
+
         # 判断是不是最后一页
-        max_page_count = tool.find_sub_string(photo_pagination_response.data.decode("GBK").encode("UTF-8"), '<div class="page"><span>共 <strong>', '</strong> 页')
+        max_page_count = tool.find_sub_string(photo_pagination_html, '<div class="page"><span>共 <strong>', '</strong> 页')
         if not max_page_count:
             extra_info['is_over'] = True
         elif robot.is_integer(max_page_count):
             extra_info['is_over'] = page_count >= int(max_page_count)
+    else:
+        raise robot.RobotException(robot.get_http_request_failed_reason(photo_pagination_response.status))
     photo_pagination_response.extra_info = extra_info
     return photo_pagination_response
 
@@ -90,13 +109,12 @@ class Rosi(robot.Robot):
             # 获取相册
             try:
                 album_pagination_response = get_one_page_album(page_count)
+            except robot.RobotException, e:
+                log.error("第%s页图集解析失败，原因：%s" % (page_count, e.message))
+                raise
             except SystemExit:
                 log.step("提前退出")
-                tool.process_exit()
-
-            if album_pagination_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-                log.error("第%s页图集访问失败，原因：%s" % (page_count, robot.get_http_request_failed_reason(album_pagination_response.status)))
-                break
+                raise
 
             log.trace("第%s页获取的所有图集：%s" % (page_count, album_pagination_response.extra_info["album_info_list"]))
 
@@ -116,18 +134,12 @@ class Rosi(robot.Robot):
                 while True:
                     try:
                         photo_pagination_response = get_one_page_photo(album_info["page_id"], album_page_count)
+                    except robot.RobotException, e:
+                        log.error("%s号图集第%s页解析失败，原因：%s" % (album_info["album_id"], album_page_count, e.message))
+                        raise
                     except SystemExit:
                         log.step("提前退出")
-                        tool.remove_dir_or_file(album_path)
-                        tool.process_exit()
-
-                    if photo_pagination_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-                        log.error("%s号图集第%s页访问失败，原因：%s" % (album_info["album_id"], album_page_count, robot.get_http_request_failed_reason(photo_pagination_response.status)))
-                        break
-
-                    if len(photo_pagination_response.extra_info["image_url_list"]) == 0:
-                        log.error("%s号图集第%s页解析图片失败" % (album_info["album_id"], album_page_count))
-                        break
+                        raise
 
                     log.trace("%s号图集第%s页获取的所有图集：%s" % (album_info["album_id"], album_page_count, album_pagination_response.extra_info["album_info_list"]))
 
