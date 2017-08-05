2611aaed99e77812f5a8fd10de7a0b0623a34eb9
hikaru <hikaru870806@hotmail.com>
2017-08-05 20:16:43 +0800

    解析方法中对异常格式处理，抛出异常

--- project/nanaGoGo/nanaGoGo.py
+++ project/nanaGoGo/nanaGoGo.py
@@ -31,27 +31,35 @@ def get_one_page_media(account_name, target_id):
     media_pagination_url = "https://api.7gogo.jp/web/v2/talks/%s/images?targetId=%s&limit=%s&direction=PREV" % (account_name, target_id, MESSAGE_COUNT_PER_PAGE)
     media_pagination_response = net.http_request(media_pagination_url, json_decode=True)
     extra_info = {
-        "is_error": False,  # 是不是格式不符合
         "media_info_list": [],  # 页面解析出的所有媒体信息列表
     }
     if media_pagination_response.status == net.HTTP_RETURN_CODE_SUCCEED:
-        if robot.check_sub_key(("data",), media_pagination_response.json_data):
-            for media_info in media_pagination_response.json_data["data"]:
-                extra_media_info = {
-                    "blog_id": None,  # 页面解析出的日志id
-                    "blog_body": None,  # 页面解析出的日志内容
-                    "json_data": media_info,  # 原始数据
-                }
-                if robot.check_sub_key(("post",), media_info) and robot.check_sub_key(("body", "postId"), media_info["post"]):
-                    # 获取日志id
-                    blog_id = str(media_info["post"]["postId"])
-                    if robot.is_integer(blog_id):
-                        extra_media_info["blog_id"] = blog_id
-                    # 获取日志内容
-                    extra_media_info["blog_body"] = media_info["post"]["body"]
-                extra_info["media_info_list"].append(extra_media_info)
-        else:
-            extra_info["is_error"] = True
+        if not robot.check_sub_key(("data",), media_pagination_response.json_data):
+            raise robot.RobotException("返回信息'data'字段不存在\n%s" % media_pagination_response.json_data)
+        if not isinstance(media_pagination_response.json_data["data"], list):
+            raise robot.RobotException("返回信息'data'字段类型不正确\n%s" % media_pagination_response.json_data)
+        for media_info in media_pagination_response.json_data["data"]:
+            extra_media_info = {
+                "blog_id": None,  # 日志id
+                "blog_body": None,  # 日志内容
+            }
+            if not robot.check_sub_key(("post",), media_info):
+                raise robot.RobotException("媒体信息'post'字段不存在\n%s" % media_info)
+            # 获取日志id
+            if not robot.check_sub_key(("postId",), media_info["post"]):
+                raise robot.RobotException("媒体信息'postId'字段不存在\n%s" % media_info)
+            if not robot.is_integer(media_info["post"]["postId"]):
+                raise robot.RobotException("媒体信息'postId'类型不正确n%s" % media_info)
+            extra_media_info["blog_id"] = str(media_info["post"]["postId"])
+
+            # 获取日志内容
+            if not robot.check_sub_key(("body",), media_info["post"]):
+                raise robot.RobotException("媒体信息'body'字段不存在\n%s" % media_info)
+            extra_media_info["blog_body"] = media_info["post"]["body"]
+
+            extra_info["media_info_list"].append(extra_media_info)
+    else:
+        raise robot.RobotException(robot.get_http_request_failed_reason(media_pagination_response.status))
     media_pagination_response.extra_info = extra_info
     return media_pagination_response
 
@@ -155,24 +163,17 @@ class Download(threading.Thread):
                 log.step(account_name + " 开始解析target id %s后的一页视频" % target_id)
 
                 # 获取一页媒体信息
-                media_pagination_response = get_one_page_media(account_name, target_id)
-                if media_pagination_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-                    log.error(account_name + " target id %s的媒体信息访问失败，原因：%s" % (target_id, robot.get_http_request_failed_reason(media_pagination_response.status)))
-                    tool.process_exit()
-
-                if media_pagination_response.extra_info["is_error"]:
-                    log.error(account_name + " target id %s的媒体信息解析失败" % target_id)
-                    tool.process_exit()
+                try:
+                    media_pagination_response = get_one_page_media(account_name, target_id)
+                except robot.RobotException, e:
+                    log.error(account_name + " target id %s的媒体信息访问失败，原因：%s" % (target_id, e.message))
+                    raise
 
                 # 如果为空，表示已经取完了
                 if len(media_pagination_response.extra_info["media_info_list"]) == 0:
                     break
 
                 for media_info in media_pagination_response.extra_info["media_info_list"]:
-                    if media_info["blog_id"] is None:
-                        log.error(account_name + " 媒体信息%s的日志id解析失败" % media_info["json_data"])
-                        tool.process_exit()
-
                     # 检查是否达到存档记录
                     if int(media_info["blog_id"]) <= int(self.account_info[3]):
                         is_over = True
@@ -186,6 +187,7 @@ class Download(threading.Thread):
                     target_id = media_info["blog_id"]
 
                     log.step(account_name + " 开始解析日志%s" % media_info["blog_id"])
+                    # todo 放到解析方法get_one_page_media()中去
                     for blog_body in media_info["blog_body"]:
                         if not robot.check_sub_key(("bodyType",), blog_body):
                             log.error(account_name + " 媒体信息%s的bodyType解析失败" % media_info["json_data"])
