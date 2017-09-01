71b136a2c9aa13d998aae58acb1a2a9e3d1a8793
hikaru <hikaru870806@hotmail.com>
2017-09-01 23:54:10 +0800

    fix 文件类型

--- project/kg/kg.py
+++ project/kg/kg.py
@@ -217,7 +217,7 @@ class Download(threading.Thread):
 
                 log.step(account_name + " 开始下载歌曲%s《%s》 %s" % (audio_info["audio_key"], audio_info["audio_title"], audio_play_response["audio_url"]))
 
-                file_type = audio_play_response["audio_url"].split(".")[-1].split("?")[0]
+                file_type = audio_play_response["audio_url"].split(".")[-1].split("?")[0].split("&")[0]
                 file_path = os.path.join(VIDEO_DOWNLOAD_PATH, account_name, "%s - %s.%s" % (audio_info["audio_id"], robot.filter_text(audio_info["audio_title"]), file_type))
                 save_file_return = net.save_net_file(audio_play_response["audio_url"], file_path)
                 if save_file_return["status"] == 1:
