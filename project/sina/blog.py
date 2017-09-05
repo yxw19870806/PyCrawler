53a785e75619a6a96f63117568b7d3a213c9f354
hikaru <hikaru870806@hotmail.com>
2017-09-05 11:12:33 +0800

    fix key error

--- project/sina/blog.py
+++ project/sina/blog.py
@@ -282,7 +282,7 @@ class Download(threading.Thread):
         self.thread_lock.acquire()
         tool.write_file("\t".join(self.account_info), NEW_SAVE_DATA_PATH)
         TOTAL_IMAGE_COUNT += total_image_count
-        ACCOUNTS.remove(account_name)
+        ACCOUNTS.remove(account_id)
         self.thread_lock.release()
         log.step(account_name + " 下载完毕，总共获得%s张图片" % total_image_count)
 
