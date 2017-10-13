523d796f163a1ff2ecd327ec694fcaf16f3ff9c4
hikaru <hikaru870806@hotmail.com>
2017-10-13 18:26:06 +0800

    优化，使用公共类

--- game/clickHero/autoUpgrade.py
+++ game/clickHero/autoUpgrade.py
@@ -5,6 +5,7 @@ clicker heroes自动升级树精
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
+from common import process, tool
 from game.clickHero import clickerHeroes
 import time
 
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     click_button_index = 0
     # (点击升级的按钮位置，1 ~ 4），保证升级窗口中第一个按钮完整显示）
     while not 1 <= int(click_button_index) <= 4:
-        input_str = clickerHeroes.console_input("请输入点击的按钮位置（1-4）: ")
+        input_str = tool.console_input("请输入点击的按钮位置（1-4）: ")
         if str(input_str).isdigit():
             click_button_index = int(input_str)
 
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     click_x, click_y = clickerHeroes.UPGRADE_BUTTON_POS[click_button_index]
     is_open_equip_box = False
     while True:
-        while clickerHeroes.PROCESS_STATUS == clickerHeroes.PROCESS_STATUS_PAUSE:
+        while process.PROCESS_STATUS == process.PROCESS_STATUS_PAUSE:
             time.sleep(1)
         # 自动升级
         ch.auto_click(click_x, click_y)
@@ -59,10 +60,10 @@ if __name__ == "__main__":
                 # 检测宝箱，并且只要开启过一次后就不再检测
                 if not is_open_equip_box and check_relic_box():
                     is_open_equip_box = True
-                    clickerHeroes.print_msg("open relic box")
+                    tool.print_msg("open relic box")
 
                 if check_progression_mode():
-                    clickerHeroes.print_msg("enable progression mode")
+                    tool.print_msg("enable progression mode")
             # 重置计数
             count = 0
 
--- game/clickHero/clickerHeroes.py
+++ game/clickHero/clickerHeroes.py
@@ -5,14 +5,10 @@ clicker heroes窗口处理类
 email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
-from common import keyboardEvent
-import pywintypes
-import sys
-import win32api
-import win32con
-import win32gui
+from common import windowsApplication
 
-
+# 窗口标题
+WINDOW_TITLE = "Clicker Heroes"
 # 每个升级按钮对应的坐标
 UPGRADE_BUTTON_POS = {
     1: (100, 230),
@@ -38,128 +34,8 @@ PROGRESSION_MODE_CHECK_POSITION = (
 MONSTER_CLICK_POSITION = (855, 395)
 DEFAULT_WINDOWS_SIZE = (1152, 678)
 DEFAULT_CLIENT_SIZE = (1136, 640)
-PROCESS_STATUS_PAUSE = 0  # 进程暂停
-PROCESS_STATUS_RUN = 1  # 进程运行
-PROCESS_STATUS = PROCESS_STATUS_RUN  # 当前进程状态
 
 
-# 设置暂停状态
-def pause_process():
-    global PROCESS_STATUS
-    if PROCESS_STATUS != PROCESS_STATUS_PAUSE:
-        print_msg("pause process")
-        PROCESS_STATUS = PROCESS_STATUS_PAUSE
-
-
-# 设置运行状态
-def continue_process():
-    global PROCESS_STATUS
-    if PROCESS_STATUS != PROCESS_STATUS_RUN:
-        print_msg("continue process")
-        PROCESS_STATUS = PROCESS_STATUS_RUN
-
-
-# 输出文字
-def print_msg(msg):
-    # 终端输出编码
-    output_encoding = sys.stdout.encoding
-    if output_encoding == "UTF-8":
-        print msg
-    else:
-        print msg.decode("UTF-8").encode(output_encoding)
-
-
-# 控制台输入
-def console_input(msg):
-    output_encoding = sys.stdout.encoding
-    if output_encoding != "UTF-8":
-        msg = msg.decode("UTF-8").encode(output_encoding)
-    return raw_input(msg)
-
-
-class ClickerHeroes:
-    window_title = "Clicker Heroes"
-
-    @property
-    def window_handle(self):
-        return win32gui.FindWindow(None, self.window_title)
-
+class ClickerHeroes(windowsApplication.WindowsApplication):
     def __init__(self):
-        # 设置为默认窗口大小（避免坐标产生偏移）
-        self.set_window_size(DEFAULT_WINDOWS_SIZE[0], DEFAULT_WINDOWS_SIZE[1])
-        keyboard_event_bind = {"Prior": pause_process, "Next": continue_process}
-        keyboard_control_thread = keyboardEvent.KeyboardEvent(keyboard_event_bind)
-        keyboard_control_thread.setDaemon(True)
-        keyboard_control_thread.start()
-        print_msg("开始运行，键盘按下pageUp按钮暂停，pageOn按钮继续")
-
-    # 获取窗口大小
-    def get_window_size(self):
-        win_rect = win32gui.GetWindowRect(self.window_handle)
-        return win_rect[2] - win_rect[0], win_rect[3] - win_rect[1]  # width, height
-
-    # 获取显示大小（去除windows标题栏和边框的尺寸）
-    def get_client_size(self):
-        win_rect = win32gui.GetClientRect(self.window_handle)
-        return win_rect[2] - win_rect[0], win_rect[3] - win_rect[1]  # width, height
-
-    # win32gui.SetWindowPos参数详解
-    # 第一个参数：窗口句柄
-    # 第二个参数：窗口层级
-    #   win32con.HWND_TOP：将窗口置于Z序的顶部。
-    #   win32con.HWND_BOTTOM：将窗口置于Z序的底部。如果参数hWnd标识了一个顶层窗口，则窗口失去顶级位置，并且被置在其他窗口的底部。
-    #   win32con.HWND_NOTOPMOST：将窗口置于所有非顶层窗口之上（即在所有顶层窗口之后）。如果窗口已经是非顶层窗口则该标志不起作用。
-    #   win32con.HWND_TOPMOST：将窗口置于所有非顶层窗口之上。即使窗口未被激活窗口也将保持顶级位置。
-    # 第三个参数：窗口X坐标
-    # 第四个参数：窗口Y坐标
-    # 第五个参数：窗口宽度
-    # 第六个参数：窗口高度
-    # 第七个参数：窗口尺寸和定位的标志
-    #   win32con.SWP_ASNCWINDOWPOS：如果调用进程不拥有窗口，系统会向拥有窗口的线程发出需求。这就防止调用线程在其他线程处理需求的时候发生死锁。
-    #   win32con.SWP_DEFERERASE：防止产生WM_SYNCPAINT消息。
-    #   win32con.SWP_DRAWFRAME：在窗口周围画一个边框（定义在窗口类描述中）。
-    #   win32con.SWP_FRAMECHANGED：给窗口发送WM_NCCALCSIZE消息，即使窗口尺寸没有改变也会发送该消息。如果未指定这个标志，只有在改变了窗口尺寸时才发送WM_NCCALCSIZE。
-    #   win32con.SWP_HIDEWINDOW：隐藏窗口。
-    #   win32con.SWP_NOACTIVATE：不激活窗口。如果未设置标志，则窗口被激活，并被设置到其他最高级窗口或非最高级组的顶部（根据参数hWndlnsertAfter设置）。
-    #   win32con.SWP_NOCOPYBITS：清除客户区的所有内容。如果未设置该标志，客户区的有效内容被保存并且在窗口尺寸更新和重定位后拷贝回客户区。
-    #   win32con.SWP_NOMOVE：维持当前位置（忽略第3个和第4个参数）。
-    #   win32con.SWP_NOOWNERZORDER：不改变z序中的所有者窗口的位置。
-    #   win32con.SWP_NOREDRAW：不重画改变的内容。如果设置了这个标志，则不发生任何重画动作。适用于客户区和非客户区（包括标题栏和滚动条）和任何由于窗回移动而露出的父窗口的所有部分。如果设置了这个标志，应用程序必须明确地使窗口无效并区重画窗口的任何部分和父窗口需要重画的部分。
-    #   win32con.SWP_NOREPOSITION：与SWP_NOOWNERZORDER标志相同。
-    #   win32con.SWP_NOSENDCHANGING：防止窗口接收WM_WINDOWPOSCHANGING消息。
-    #   win32con.SWP_NOSIZE：维持当前尺寸（忽略第5个和第6个参数）。
-    #   win32con.SWP_NOZORDER：维持当前Z序（忽略第2个参数）。
-    #   win32con.SWP_SHOWWINDOW：显示窗口。
-    # 设置窗口大小
-    def set_window_size(self, width, height):
-        win32gui.SetWindowPos(self.window_handle, 0, 0, 0, width, height, win32con.SWP_NOMOVE | win32con.SWP_NOZORDER)
-
-    # 设置窗口坐标
-    def set_window_pos(self, pos_x, pos_y):
-        win32gui.SetWindowPos(self.window_handle, 0, pos_x, pos_y, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOZORDER)
-
-    # 自动点击窗口某个坐标（窗口可以不在最顶端）
-    def auto_click(self, pos_x, pos_y):
-        tmp = win32api.MAKELONG(pos_x, pos_y)
-        win32gui.SendMessage(self.window_handle, win32con.WM_ACTIVATE, win32con.WA_ACTIVE, 0)
-        win32gui.SendMessage(self.window_handle, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, tmp)
-        win32gui.SendMessage(self.window_handle, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, tmp)
-
-    # 获取窗口某个坐标的颜色（窗口必须在最顶端）
-    def get_color(self, pos_x, pos_y):
-        try:
-            color = win32gui.GetPixel(win32gui.GetDC(self.window_handle), pos_x, pos_y)
-        except pywintypes.error:
-            return None, None, None
-        red = color & 255
-        green = (color >> 8) & 255
-        blue = (color >> 16) & 255
-        return red, green, blue
-
-    # 判断是否是最顶端窗口
-    def is_foreground_window(self):
-        return win32gui.GetForegroundWindow() == self.window_handle
-
-    # 根据屏幕坐标获取对应窗口坐标
-    def get_client_position(self, pos_x, pos_y):
-        return win32gui.ScreenToClient(self.window_handle, (pos_x, pos_y))
+        windowsApplication.WindowsApplication.__init__(self, WINDOW_TITLE, DEFAULT_WINDOWS_SIZE)
