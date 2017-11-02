e989b88744749ff09dac05c175f20b5fa2d15fc8
hikaru <hikaru870806@hotmail.com>
2017-11-03 00:33:07 +0800

    无效的图片

--- project/tumblr/tumblr.py
+++ project/tumblr/tumblr.py
@@ -139,7 +139,7 @@ def get_post_page(post_url, is_safe_mode):
         new_image_url_list = {}
         for image_url in image_url_list:
             # 头像，跳过
-            if image_url.find("/avatar_") != -1:
+            if image_url.find("/avatar_") != -1 or image_url.find("_75sq.gif") != -1:
                 continue
             image_id, resolution = analysis_image(image_url)
             # 判断是否有分辨率更小的相同图片
