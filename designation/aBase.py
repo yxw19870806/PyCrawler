af559a26f30d7c030525c66af09d408477572033
hikaru <hikaru870806@hotmail.com>
2017-11-02 19:11:27 +0800

    使用自带的urlencode方法

--- designation/aBase.py
+++ designation/aBase.py
@@ -15,8 +15,9 @@ TOTAL_IMAGE_COUNT = 0
 
 # 获取指定页数的全部图片
 def get_one_page_photo(page_count):
-    photo_pagination_url = "http://abase.me/movies.php?page=%s" % page_count
-    photo_pagination_response = net.http_request(photo_pagination_url, method="GET")
+    photo_pagination_url = "http://abase.me/movies.php"
+    query_data = {"page": page_count}
+    photo_pagination_response = net.http_request(photo_pagination_url, method="GET", fields=query_data)
     result = {
         "page_video_count": 0,  # 页面解析出的影片数量
         "image_info_list": [],  # 页面解析出的图片信息列表
