4d37e8eb1b1d034f048b4434caa185953cb3e3e2
hikaru <hikaru870806@hotmail.com>
2017-11-29 22:26:23 +0800

    500系列报错累加错误次数的退出

--- common/net.py
+++ common/net.py
@@ -204,7 +204,7 @@ def http_request(url, method="GET", fields=None, binary_data=None, header_list=N
                                 is_error = False
                     if is_error:
                         response.status = HTTP_RETURN_CODE_JSON_DECODE_ERROR
-            elif response.status in [500, 502, 503, 504] and is_auto_retry:  # 服务器临时性错误，重试
+            elif response.status in [500, 502, 503, 504] and is_auto_retry and retry_count < HTTP_REQUEST_RETRY_COUNT:  # 服务器临时性错误，重试
                 retry_count += 1
                 continue
             return response
