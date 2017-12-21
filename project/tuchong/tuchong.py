094ece469575c10ef56cb0b057907e57d989efbf
hikaru <hikaru870806@hotmail.com>
2017-12-21 22:04:52 +0800

    没有图片的处理

--- project/tuchong/tuchong.py
+++ project/tuchong/tuchong.py
@@ -77,13 +77,23 @@ def get_one_page_album(account_id, post_time):
             raise robot.RobotException("相册信息'post_id'字段类型不正确\n%s" % album_info)
         result_image_info["album_id"] = str(album_info["post_id"])
         # 获取相册标题
+        if not robot.check_sub_key(("title",), album_info):
+            raise robot.RobotException("相册信息'title'字段不存在\n%s" % album_info)
         result_image_info["album_title"] = str(album_info["title"].encode("UTF-8"))
         # 获取图片地址
+        if not robot.check_sub_key(("image_count", "images"), album_info):
+            raise robot.RobotException("相册信息'image_count'或'images'字段不存在\n%s" % album_info)
+        if not robot.check_sub_key(("image_count",), album_info):
+            raise robot.RobotException("相册信息'image_count'字段类型不正确\n%s" % album_info)
+        if not isinstance(album_info["images"], list):
+            raise robot.RobotException("相册信息'images'字段类型不正确\n%s" % album_info)
+        if len(album_info["images"]) != int(album_info["image_count"]):
+            raise robot.RobotException("相册信息'images'长度和'image_count'数值不匹配\n%s" % album_info)
         for image_info in album_info["images"]:
             if not robot.check_sub_key(("img_id",), image_info):
                 raise robot.RobotException("相册信息'img_id'字段不存在\n%s" % album_info)
             result_image_info["image_url_list"].append("https://photo.tuchong.com/%s/f/%s.jpg" % (account_id, str(image_info["img_id"])))
-        if len(result_image_info["image_url_list"]) == 0:
+        if int(album_info["image_count"]) > 0 and len(result_image_info["image_url_list"]) == 0:
             raise robot.RobotException("相册信息匹配图片地址失败\n%s" % album_info)
         # 获取相册创建时间
         if not robot.check_sub_key(("published_at",), album_info):
