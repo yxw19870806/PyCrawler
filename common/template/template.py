1d745a57ae39900e85dd66d04c2367846a050027
hikaru <hikaru870806@hotmail.com>
2017-12-28 17:08:06 +0800

    模板更新

--- common/template/template.py
+++ common/template/template.py
@@ -13,15 +13,6 @@ import threading
 import time
 import traceback
 
-ACCOUNT_LIST = {}
-TOTAL_IMAGE_COUNT = 0
-TOTAL_VIDEO_COUNT = 0
-IMAGE_DOWNLOAD_PATH = ""
-VIDEO_DOWNLOAD_PATH = ""
-NEW_SAVE_DATA_PATH = ""
-IS_DOWNLOAD_IMAGE = True
-IS_DOWNLOAD_VIDEO = True
-
 
 # 获取一页日志
 def get_one_page_blog(account_id, page_count):
@@ -42,12 +33,6 @@ def get_blog_page(account_id, blog_id):
 
 class Template(robot.Robot):
     def __init__(self):
-        global IMAGE_DOWNLOAD_PATH
-        global VIDEO_DOWNLOAD_PATH
-        global NEW_SAVE_DATA_PATH
-        global IS_DOWNLOAD_IMAGE
-        global IS_DOWNLOAD_VIDEO
-
         # todo 配置
         sys_config = {
             robot.SYS_DOWNLOAD_IMAGE: True,
@@ -57,25 +42,15 @@ class Template(robot.Robot):
         }
         robot.Robot.__init__(self, sys_config)
 
-        # 设置全局变量，供子线程调用
-        # todo 是否需要下载图片或视频
-        IMAGE_DOWNLOAD_PATH = self.image_download_path
-        VIDEO_DOWNLOAD_PATH = self.video_download_path
-        IS_DOWNLOAD_IMAGE = self.is_download_image
-        IS_DOWNLOAD_VIDEO = self.is_download_video
-        NEW_SAVE_DATA_PATH = robot.get_new_save_file_path(self.save_data_path)
-
     def main(self):
-        global ACCOUNT_LIST
-
         # todo 存档文件格式
         # 解析存档文件
         # account_id
-        ACCOUNT_LIST = robot.read_save_data(self.save_data_path, 0, ["", ])
+        self.account_list = robot.read_save_data(self.save_data_path, 0, ["", ])
 
         # 循环下载每个id
         main_thread_count = threading.activeCount()
-        for account_id in sorted(ACCOUNT_LIST.keys()):
+        for account_id in sorted(self.account_list.keys()):
             # 检查正在运行的线程数
             if threading.activeCount() >= self.thread_count + main_thread_count:
                 self.wait_sub_thread()
@@ -85,7 +60,7 @@ class Template(robot.Robot):
                 break
 
             # 开始下载
-            thread = Download(ACCOUNT_LIST[account_id], self)
+            thread = Download(self.account_list[account_id], self)
             thread.start()
 
             time.sleep(1)
@@ -95,14 +70,14 @@ class Template(robot.Robot):
             self.wait_sub_thread()
 
         # 未完成的数据保存
-        if len(ACCOUNT_LIST) > 0:
-            tool.write_file(tool.list_to_string(ACCOUNT_LIST.values()), NEW_SAVE_DATA_PATH)
+        if len(self.account_list) > 0:
+            tool.write_file(tool.list_to_string(self.account_list.values()), self.temp_save_data_path)
 
         # 重新排序保存存档文件
-        robot.rewrite_save_file(NEW_SAVE_DATA_PATH, self.save_data_path)
+        robot.rewrite_save_file(self.temp_save_data_path, self.save_data_path)
 
         # todo 是否需要下载图片或视频
-        log.step("全部下载完毕，耗时%s秒，共计图片%s张，视频%s个" % (self.get_run_time(), TOTAL_IMAGE_COUNT, TOTAL_VIDEO_COUNT))
+        log.step("全部下载完毕，耗时%s秒，共计图片%s张，视频%s个" % (self.get_run_time(), self.total_image_count, self.total_video_count))
 
 
 class Download(robot.DownloadThread):
@@ -157,13 +132,13 @@ class Download(robot.DownloadThread):
         # todo 图片下载逻辑
         # 图片下载
         image_index = self.account_info[1] + 1
-        if IS_DOWNLOAD_IMAGE:
+        if self.main_thread.is_download_image:
             for image_url in blog_response["image_url_list"]:
                 self.main_thread_check()  # 检测主线程运行状态
                 log.step(self.account_name + " 开始下载第%s张图片 %s" % (image_index, image_url))
 
                 file_type = image_url.split(".")[-1]
-                image_file_path = os.path.join(IMAGE_DOWNLOAD_PATH, self.account_name, "%04d.%s" % (image_index, file_type))
+                image_file_path = os.path.join(self.main_thread.image_download_path, self.account_name, "%04d.%s" % (image_index, file_type))
                 save_file_return = net.save_net_file(image_url, image_file_path)
                 if save_file_return["status"] == 1:
                     # 设置临时目录
@@ -176,13 +151,13 @@ class Download(robot.DownloadThread):
         # todo 视频下载逻辑
         # 视频下载
         video_index = self.account_info[2] + 1
-        if IS_DOWNLOAD_VIDEO:
+        if self.main_thread.is_download_video:
             for video_url in blog_response["video_url_list"]:
                 self.main_thread_check()  # 检测主线程运行状态
                 log.step(self.account_name + " 开始下载第%s个视频 %s" % (video_index, video_url))
 
                 file_type = video_url.split(".")[-1]
-                video_file_path = os.path.join(VIDEO_DOWNLOAD_PATH, self.account_name, "%04d.%s" % (video_index, file_type))
+                video_file_path = os.path.join(self.main_thread.video_download_path, self.account_name, "%04d.%s" % (video_index, file_type))
                 save_file_return = net.save_net_file(video_url, video_file_path)
                 if save_file_return["status"] == 1:
                     # 设置临时目录
@@ -225,12 +200,10 @@ class Download(robot.DownloadThread):
 
         # 保存最后的信息
         with self.thread_lock:
-            global TOTAL_IMAGE_COUNT
-            global TOTAL_VIDEO_COUNT
-            tool.write_file("\t".join(self.account_info), NEW_SAVE_DATA_PATH)
-            TOTAL_IMAGE_COUNT += self.total_image_count
-            TOTAL_VIDEO_COUNT += self.total_video_count
-            ACCOUNT_LIST.pop(self.account_id)
+            tool.write_file("\t".join(self.account_info), self.main_thread.temp_save_data_path)
+            self.main_thread.total_image_count += self.total_image_count
+            self.main_thread.total_video_count += self.total_video_count
+            self.main_thread.account_list.pop(self.account_id)
         log.step(self.account_name + " 下载完毕，总共获得%s张图片和%s个视频" % (self.total_image_count, self.total_video_count))
         self.notify_main_thread()
 
