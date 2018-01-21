659601371c642401e13fbb9557cbc709111c67ad
hikaru <hikaru870806@hotmail.com>
2018-01-21 17:13:01 +0800

    多视频地址的支持&位置来源不影响记录

--- project/ivseek/ivseek.py
+++ project/ivseek/ivseek.py
@@ -15,9 +15,8 @@ def get_archive_page(archive_id):
     archive_url = "http://www.ivseek.com/archives/%s.html" % archive_id
     archive_response = net.http_request(archive_url, method="GET")
     result = {
-        "account_id": "",  # 视频发布账号（youtube）
         "title": "",  # 标题
-        "video_url": None,  # 视频地址（youtube）
+        "video_info_list": [],  # 全部视频信息
     }
     if archive_response.status == 404:
         return result
@@ -35,32 +34,39 @@ def get_archive_page(archive_id):
     video_url_find = re.findall('<iframe[\s|\S]*?src="([^"]*)"', archive_response.data)
     if len(video_url_find) == 0:
         raise crawler.CrawlerException("视频地址匹配失败")
-    elif len(video_url_find) != 1:
-        log.error("多个内嵌iframe" + str(video_url_find))
-    video_url = str(video_url_find[0])
-    # 'http://embed.share-videos.se/auto/embed/40537536?uid=6050'
-    if video_url.find("//embed.share-videos.se/") >= 0:
-        video_id = video_url.split("/")[-1]
-        result["video_url"] = "http://share-videos.se/auto/video/%s" % video_id
-    # https://www.youtube.com/embed/9GSEOmLD_zc?feature=oembed
-    elif video_url.find("//www.youtube.com/") >= 0:
-        video_id = video_url.split("/")[-1].split("?")[0]
-        result["video_url"] = "https://www.youtube.com/watch?v=%s" % video_id
-        # 获取视频发布账号
-        header_list = {
-            "cookie": "YSC=5qtzcCE2QUY; VISITOR_INFO1_LIVE=zldN2C7424k; PREF=f1=50000000&f5=30000",
-            "x-client-data": "CJK2yQEIpLbJAQjEtskBCPqcygEIqZ3KAQioo8oB",
-            "accept-language": "zh-CN,zh;q=0.9",
+    for video_url in video_url_find:
+        result_video_info = {
+            "account_id": "",  # 视频发布账号（youtube）
+            "video_url": None,  # 视频信息
         }
-        video_play_response = net.http_request(result["video_url"], method="GET", header_list=header_list)
-        if video_play_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-            raise crawler.CrawlerException("视频播放页面%s %s" % (result["video_url"], crawler.request_failre(archive_response.status)))
-        account_id = tool.find_sub_string(video_play_response.data, '"webNavigationEndpointData":{"url":"/channel/', '"')
-        if not account_id:
-            raise crawler.CrawlerException("视频发布账号截取失败")
-        result["account_id"] = str(account_id)
-    else:
-        raise crawler.CrawlerException("未知视频来源" + video_url)
+        # 'http://embed.share-videos.se/auto/embed/40537536?uid=6050'
+        if video_url.find("//embed.share-videos.se/") >= 0:
+            video_id = video_url.split("/")[-1]
+            result_video_info["video_url"] = "http://share-videos.se/auto/video/%s" % video_id
+        # https://www.youtube.com/embed/9GSEOmLD_zc?feature=oembed
+        elif video_url.find("//www.youtube.com/") >= 0:
+            video_id = video_url.split("/")[-1].split("?")[0]
+            result_video_info["video_url"] = "https://www.youtube.com/watch?v=%s" % video_id
+            # 获取视频发布账号
+            header_list = {
+                "cookie": "YSC=5qtzcCE2QUY; VISITOR_INFO1_LIVE=zldN2C7424k; PREF=f1=50000000&f5=30000",
+                "x-client-data": "CJK2yQEIpLbJAQjEtskBCPqcygEIqZ3KAQioo8oB",
+                "accept-language": "zh-CN,zh;q=0.9",
+            }
+            video_play_response = net.http_request(result_video_info["video_url"], method="GET", header_list=header_list)
+            if video_play_response.status != net.HTTP_RETURN_CODE_SUCCEED:
+                raise crawler.CrawlerException("视频播放页面%s %s" % (result["video_url"], crawler.request_failre(archive_response.status)))
+            account_id = tool.find_sub_string(video_play_response.data, '"webNavigationEndpointData":{"url":"/channel/', '"')
+            if not account_id:
+                raise crawler.CrawlerException("视频发布账号截取失败")
+            result_video_info["account_id"] = str(account_id)
+        # 无效的视频地址
+        elif video_url.find("//rcm-fe.amazon-adsystem.com") >= 0:
+            continue
+        else:
+            result_video_info["video_url"] = video_url
+            log.error("未知视频来源" + video_url)
+        result["video_info_list"].append(result_video_info)
     return result
 
 
@@ -90,10 +96,11 @@ class IvSeek(crawler.Crawler):
                 archive_id += 1
                 continue
 
-            if archive_response["video_url"] is None:
+            if len(archive_response["video_info_list"]) == 0:
                 continue
 
-            tool.write_file("%s\t%s\t%s\t%s" % (archive_id, archive_response["title"], archive_response["video_url"], archive_response["account_id"]), self.save_data_path)
+            for video_info in archive_response["video_info_list"]:
+                tool.write_file("%s\t%s\t%s\t%s" % (archive_id, archive_response["title"], video_info["video_url"], video_info["account_id"]), self.save_data_path)
 
             # 提前结束
             if not self.is_running():
