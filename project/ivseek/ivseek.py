63be7b50c69d797f5c9b452896b78eb6181e22ad
hikaru <hikaru870806@hotmail.com>
2018-01-22 10:22:38 +0800

    增加最新视频id获取&增加异常捕获

--- project/ivseek/ivseek.py
+++ project/ivseek/ivseek.py
@@ -5,10 +5,24 @@ email: hikaru870806@hotmail.com
 如有问题或建议请联系
 """
 from common import *
-import os
 import re
-import time
-import threading
+import traceback
+
+
+# 获取首页
+def get_index_page():
+    index_url = "http://www.ivseek.com/"
+    index_response = net.http_request(index_url, method="GET")
+    result = {
+        "max_archive_id": None,  # 最新图集id
+    }
+    if index_response.status != net.HTTP_RETURN_CODE_SUCCEED:
+        raise crawler.CrawlerException(crawler.request_failre(index_response.status))
+    archive_id_find = re.findall('<a class="no-deco" href="http://www.ivseek.com/archives/(\d*).html">', index_response.data)
+    if len(archive_id_find) == 0:
+        raise crawler.CrawlerException("页面匹配视频id失败\n%s" % index_response.data)
+    result["max_archive_id"] = max(map(int, archive_id_find))
+    return result
 
 
 def get_archive_page(archive_id):
@@ -50,22 +64,17 @@ def get_archive_page(archive_id):
                 continue
             account_id = tool.find_sub_string(video_play_response.data, '"webNavigationEndpointData":{"url":"/channel/', '"')
             if not account_id:
-                raise crawler.CrawlerException("视频 %s 发布账号截取失败" % result_video_info["video_url"])
+                raise crawler.CrawlerException("视频 %s 发布账号截取失败\n%s" % (result_video_info["video_url"], video_play_response.data))
             result_video_info["account_id"] = str(account_id)
-        # https://embed.nicovideo.jp/watch/sm23008734/script?w=640&#038;h=360
         elif video_url.find("embed.nicovideo.jp/watch") >= 0:
-            video_id = video_url.split("/")[-2]
-            result_video_info["video_url"] = "http://www.nicovideo.jp/watch/%s" % video_id
-            # 获取视频发布账号
-            video_play_response = net.http_request(result_video_info["video_url"], method="GET", header_list={"accept-language": "en-US"})
-            if video_play_response.status != net.HTTP_RETURN_CODE_SUCCEED:
-                raise crawler.CrawlerException("视频播放页%s，%s" % (result["video_url"], crawler.request_failre(video_play_response.status)))
-            account_id = tool.find_sub_string(video_play_response.data, '<a href="/user/', '"')
-            if crawler.is_integer(account_id):
-                result_video_info["account_id"] = str(account_id)
-        # http://ext.nicovideo.jp/thumb_watch/sm21088018?w=490&#038;h=307
-        elif video_url.find("ext.nicovideo.jp/thumb_watch/") >= 0:
-            video_id = video_url.split("/")[-1].split("?")[0]
+            # https://embed.nicovideo.jp/watch/sm23008734/script?w=640&#038;h=360
+            if video_url.find("embed.nicovideo.jp/watch") >= 0:
+                video_id = video_url.split("/")[-2]
+            # http://ext.nicovideo.jp/thumb_watch/sm21088018?w=490&#038;h=307
+            elif video_url.find("ext.nicovideo.jp/thumb_watch/") >= 0:
+                video_id = video_url.split("/")[-1].split("?")[0]
+            else:
+                raise crawler.CrawlerException("未知视频来源" + video_url)
             result_video_info["video_url"] = "http://www.nicovideo.jp/watch/%s" % video_id
             # 获取视频发布账号
             video_play_response = net.http_request(result_video_info["video_url"], method="GET", header_list={"accept-language": "en-US"})
@@ -104,26 +113,44 @@ class IvSeek(crawler.Crawler):
         if len(save_info_list) > 0:
             save_id = int(save_info_list[-1].split("\t")[0]) + 1
 
-        for archive_id in range(save_id, 3000):
-            log.step("开始解析第%s页视频" % archive_id)
-
-            # 获取一页图片
+        try:
+            # 获取首页
             try:
-                archive_response = get_archive_page(archive_id)
+                index_response = get_index_page()
             except crawler.CrawlerException, e:
-                log.error("第%s页视频解析失败，原因：%s" % (archive_id, e.message))
-                archive_id += 1
-                continue
+                log.error("首页解析失败，原因：%s" % e.message)
+                raise
 
-            if len(archive_response["video_info_list"]) == 0:
-                continue
+            log.step("最新视频id：%s" % index_response["max_archive_id"])
 
-            for video_info in archive_response["video_info_list"]:
-                tool.write_file("%s\t%s\t%s\t%s" % (archive_id, archive_response["title"], video_info["video_url"], video_info["account_id"]), self.save_data_path)
+            for archive_id in range(save_id, index_response["max_archive_id"]):
+                log.step("开始解析第%s个视频" % archive_id)
 
-            # 提前结束
-            if not self.is_running():
-                break
+                # 获取一页图片
+                try:
+                    archive_response = get_archive_page(archive_id)
+                except crawler.CrawlerException, e:
+                    log.error("第%s个视频解析失败，原因：%s" % (archive_id, e.message))
+                    archive_id += 1
+                    continue
+
+                if len(archive_response["video_info_list"]) == 0:
+                    continue
+
+                for video_info in archive_response["video_info_list"]:
+                    tool.write_file("%s\t%s\t%s\t%s" % (archive_id, archive_response["title"], video_info["video_url"], video_info["account_id"]), self.save_data_path)
+
+                # 提前结束
+                if not self.is_running():
+                    break
+        except SystemExit, se:
+            if se.code == 0:
+                log.step("提前退出")
+            else:
+                log.error("异常退出")
+        except Exception, e:
+            log.error("未知异常")
+            log.error(str(e) + "\n" + str(traceback.format_exc()))
 
 
 if __name__ == "__main__":
