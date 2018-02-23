72f19883664bda766c64993582bb27c4e13efa7b
hikaru <hikaru870806@hotmail.com>
2018-02-23 14:39:26 +0800

    update

--- project/meituri/meituri.py
+++ project/meituri/meituri.py
@@ -117,10 +117,7 @@ class MeiTuRi(crawler.Crawler):
 
                     file_type = image_url.split(".")[-1]
                     file_path = os.path.join(album_path, "%03d.%s" % (image_index, file_type))
-                    header_list = {
-                        "Referer": "http://www.meituri.com/"
-                    }
-                    save_file_return = net.save_net_file(image_url, file_path, header_list=header_list)
+                    save_file_return = net.save_net_file(image_url, file_path, header_list={"Referer": "http://www.meituri.com/"})
                     if save_file_return["status"] == 1:
                         log.step("图集%s 《%s》 第%s张图片下载成功" % (album_id, album_title, image_index))
                     else:
