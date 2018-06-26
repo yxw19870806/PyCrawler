40454fa07b4a5dc2bfb57bb3c815022ee74cb321
hikaru <hikaru870806@hotmail.com>
2018-06-26 15:04:06 +0800

    增加端口事件监听，暂停/重启/结束进程

--- common/process.py
+++ common/process.py
@@ -6,34 +6,30 @@ email: hikaru870806@hotmail.com
 """
 import threading
 from multiprocessing.connection import Client, Listener
-from common import output
 
 PROCESS_SERVER_IP = "localhost"  # 监听服务器IP
 PROCESS_SERVER_PORT = 12345  # 监听服务器端口
-PROCESS_STATUS = 0  # 服务器当前状态
 PROCESS_STATUS_RUN = 0  # 进程运行中
 PROCESS_STATUS_PAUSE = 1  # 进程暂停，知道状态变为0时才继续下载
 PROCESS_STATUS_STOP = 2  # 进程立刻停止，删除还未完成的数据
-PROCESS_STATUS_FINISH = 3  # 进程等待现有任务完成后停止
-PROCESS_PID_LIST = []  # 存放所有需要操作的进程PID
 
 
 class ProcessControl(threading.Thread):
     """program status controller Class"""
-    def __init__(self, ip=PROCESS_SERVER_IP, port=PROCESS_SERVER_PORT):
+    def __init__(self, port=PROCESS_SERVER_PORT, event_list=None):
         threading.Thread.__init__(self)
-        self.ip = str(ip)
+        self.ip = PROCESS_SERVER_IP
         self.port = int(port)
+        self.event_list = event_list
 
     def run(self):
-        global PROCESS_STATUS
         listener = Listener((self.ip, self.port))
         while True:
             try:
                 conn = listener.accept()
-                new_status = int(conn.recv())
-                if new_status in [PROCESS_STATUS_RUN, PROCESS_STATUS_PAUSE, PROCESS_STATUS_FINISH, PROCESS_STATUS_STOP]:
-                    PROCESS_STATUS = new_status
+                command = int(conn.recv())
+                if command in self.event_list:
+                    self.event_list[command]()
             except IOError:
                 pass
             finally:
@@ -55,17 +51,14 @@ def set_process_status(process_status):
 
 def pause_process():
     """Pause program"""
-    output.print_msg("pause process")
     set_process_status(PROCESS_STATUS_PAUSE)
 
 
 def continue_process():
     """restart program"""
-    output.print_msg("continue process")
     set_process_status(PROCESS_STATUS_RUN)
 
 
 def stop_process():
     """Stop program"""
-    output.print_msg("stop process")
     set_process_status(PROCESS_STATUS_STOP)
